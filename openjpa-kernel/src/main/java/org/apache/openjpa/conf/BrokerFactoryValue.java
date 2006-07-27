120d99cc5205425f3834e274faa049e8c13d90eb
Patrick Linskey <pcl@apache.org>
2006-07-27 19:59:09 +0000

    removed unneeded imports
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/openjpa/trunk@426217 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/conf/BrokerFactoryValue.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/conf/BrokerFactoryValue.java
@@ -17,9 +17,6 @@ package org.apache.openjpa.conf;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.openjpa.abstractstore.AbstractStoreBrokerFactory;
