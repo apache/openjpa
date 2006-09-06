8108f4ec8dabec012324b2607c0ab3d8d2a42819
Pinaki Poddar <ppoddar@apache.org>
2006-09-06 14:23:28 +0000

    An extension mechanism for PersistenceProvider to supply their own Configuration Provider and EntityManagerFactory. This is a tentative/interim solution and likely to be replaced once we move to ProductDerivation based extension framework.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/openjpa/trunk@440721 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openjpa-persistence/src/main/java/org/apache/openjpa/persistence/PersistenceProviderExtension.java
@@ -0,0 +1,27 @@
+package org.apache.openjpa.persistence;
+
+import org.apache.openjpa.kernel.BrokerFactory;
+
+/**
+ * Allows the Persistence Provider to be supply their own Configuration 
+ * Provider and EntityManagerFactory.
+ * 
+ * @author Pinaki Poddar
+ * @since 0.4.1
+ * @nojavadoc
+ */
+public interface PersistenceProviderExtension {
+	/**
+	 * Gets the Configuration Provider implementation for this receiver.
+	 * 
+	 */
+	public ConfigurationProviderImpl newConfigurationProviderImpl();
+	
+	/**
+	 * Gets the EntityManagerFactory given a Broker Factory.
+	 * @param factory
+
+	 */
+	public OpenJPAEntityManagerFactory toEntityManagerFactory(BrokerFactory 
+		factory);
+}
