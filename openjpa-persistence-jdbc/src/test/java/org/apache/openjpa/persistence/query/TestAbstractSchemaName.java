5344ecbd412a1b01eb46519a113858060d6359a5
A. Abram White <awhite@apache.org>
2006-11-08 00:50:15 +0000

    Simple test case for abstract schema name queries.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/openjpa/trunk@472355 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/query/TestAbstractSchemaName.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openjpa.persistence.query;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+import org.apache.openjpa.persistence.OpenJPAQuery;
+
+/**
+ * Test that we can query by an entity's abstract schema name.
+ *
+ * @author Abe White
+ */
+public class TestAbstractSchemaName
+    extends TestCase {
+
+    private EntityManagerFactory emf;
+
+    public void setUp() {
+        Map props = new HashMap();
+        props.put("openjpa.MetaDataFactory", "jpa(Types=" 
+            + NamedEntity.class.getName() + ")");
+        emf = Persistence.createEntityManagerFactory("test", props);
+
+        NamedEntity e = new NamedEntity();
+        e.setName("e"); 
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void tearDown() {
+        if (emf == null)
+            return;
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.createQuery("delete from named").executeUpdate();
+            em.getTransaction().commit();
+            em.close();
+            emf.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testQuery() {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("select e from named e");
+        NamedEntity e = (NamedEntity) q.getSingleResult();
+        assertNotNull(e);
+        assertEquals("e", e.getName());
+        em.close();
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(TestAbstractSchemaName.class);
+    }
+}
+
