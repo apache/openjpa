8075b24bcab9729bc4d5ae1f7a7fc52e190b8673
Patrick Linskey <pcl@apache.org>
2007-01-10 22:42:19 +0000

    import cleanup
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/openjpa/trunk@495017 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/enhance/PCEnhancer.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/enhance/PCEnhancer.java
@@ -30,7 +30,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
