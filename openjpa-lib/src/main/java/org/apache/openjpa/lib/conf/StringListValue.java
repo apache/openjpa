2ac1bcccbb4070da2c86bfee4c7368d9466141ca
Marc Prud'hommeaux <mprudhom@apache.org>
2007-01-31 22:27:29 +0000

    OPENJPA-118: Implemented patch provided by David Ezzio for broken openjpa.AutoDetach behavior
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openjpa/trunk@502022 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-lib/src/main/java/org/apache/openjpa/lib/conf/StringListValue.java
+++ openjpa-lib/src/main/java/org/apache/openjpa/lib/conf/StringListValue.java
@@ -15,6 +15,13 @@
  */
 package org.apache.openjpa.lib.conf;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.openjpa.lib.util.Localizer;
+import org.apache.openjpa.lib.util.ParseException;
+
 import serp.util.Strings;
 
 /**
@@ -25,6 +32,8 @@ import serp.util.Strings;
 public class StringListValue extends Value {
 
     public static final String[] EMPTY = new String[0];
+    private static final Localizer s_loc = Localizer.forPackage
+        (StringListValue.class);
 
     private String[] _values = EMPTY;
 
@@ -50,6 +59,61 @@ public class StringListValue extends Value {
     public Class getValueType() {
         return String[].class;
     }
+    
+    /**
+     * Unalias the value list.  This method defers to super.unalias()
+     * UNLESS the string passed is a list of values for a property that
+     * has aliases.
+     */
+    public String unalias(String str) {
+        
+        // defer to super.unalias
+        String[] aliases = getAliases();
+        if (aliases.length <= 0 || str == null)
+            return super.unalias(str);
+        str = str.trim();
+        if (str.length() <= 0)
+            return super.unalias(str);
+        
+        // snag this case early as it only causes problems
+        if (str.equals(","))
+            throw new ParseException(s_loc.get("invalid-list-config",
+                getProperty(), str, getAliasList()));
+        
+        // unalias the list and concatenate the list of
+        // canonical values.  Also, catch any bad aliases.
+        boolean found;
+        String iString;
+        StringBuffer retv = new StringBuffer();
+        String[] vals = str.split(",", 0);
+        
+        for (int i = 0; i < vals.length; i++) {
+            iString = vals[i] = vals[i].trim();
+            
+            found = false;
+            if (i > 0)
+                retv.append(',');
+            
+            for (int x = 0; x < aliases.length; x += 2)
+                if (StringUtils.equals(iString, aliases[x])
+                    || StringUtils.equals(iString, aliases[x + 1])) {
+                    retv.append(aliases[x + 1]);
+                    found = true;
+                    break;
+                }
+            
+            // If the alias list is not comprehensive, add any unknown
+            // values back onto the list
+            if (!found) {
+                if (isAliasListComprehensive())
+                    throw new ParseException(s_loc.get("invalid-list-config",
+                        getProperty(), str, getAliasList()));
+                else
+                    retv.append(iString);
+            }
+        }
+        return retv.toString();
+    }
 
     protected String getInternalString() {
         return Strings.join(_values, ", ");
@@ -57,14 +121,21 @@ public class StringListValue extends Value {
 
     protected void setInternalString(String val) {
         String[] vals = Strings.split(val, ",", 0);
-        if (vals != null)
+        if (vals != null) {
             for (int i = 0; i < vals.length; i++)
                 vals[i] = vals[i].trim();
+        }
+
         set(vals);
     }
 
     protected void setInternalObject(Object obj) {
         set((String[]) obj);
     }
+    
+    protected List getAliasList() {
+        return Arrays.asList(getAliases());
+    }
+    
 }
 
---
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/conf/TestBadAutoDetachProperty.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openjpa.conf;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import junit.framework.*;
+
+import org.apache.openjpa.lib.util.ParseException;
+import org.apache.openjpa.persistence.*;
+import org.apache.openjpa.persistence.PersistenceException;
+
+public class TestBadAutoDetachProperty extends TestCase {
+    public void testEmptyValue() {
+        HashMap props = new HashMap(System.getProperties());
+        props.put("openjpa.AutoDetach", "");
+        EntityManagerFactory emf = (OpenJPAEntityManagerFactory) Persistence
+                .createEntityManagerFactory("test", props);
+        EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
+    }
+
+    public void testCommaOnlyValue() {
+        try {
+            HashMap props = new HashMap(System.getProperties());
+            props.put("openjpa.AutoDetach", ",");
+            EntityManagerFactory emf = (OpenJPAEntityManagerFactory) Persistence
+                    .createEntityManagerFactory("test", props);
+            EntityManager em = emf.createEntityManager();
+            em.close();
+            emf.close();
+        } catch (PersistenceException e) {
+            Throwable cause = e.getCause();
+            if (!(cause instanceof ParseException)) {
+                fail("Should have caught PersistenceException whose cause was "
+                        + "a ParseException. " + "Instead the cause was: "
+                        + cause);
+            }
+        } catch (RuntimeException e) {
+            fail("Should have caught a PersistenceException, instead caught: "
+                    + e);
+        }
+    }
+
+    public void testEmptyItemValue() {
+        try {
+            HashMap props = new HashMap(System.getProperties());
+            props.put("openjpa.AutoDetach", "close,,commit");
+            EntityManagerFactory emf = (OpenJPAEntityManagerFactory) Persistence
+                    .createEntityManagerFactory("test", props);
+            EntityManager em = emf.createEntityManager();
+            em.close();
+            emf.close();
+        } catch (PersistenceException e) {
+            Throwable cause = e.getCause();
+            if (!(cause instanceof ParseException)) {
+                fail("Should have caught PersistenceException whose cause was "
+                        + "a ParseException. " + "Instead the cause was: "
+                        + cause);
+            }
+        } catch (RuntimeException e) {
+            fail("Should have caught a PersistenceException, instead caught: "
+                    + e);
+        }
+    }
+}
