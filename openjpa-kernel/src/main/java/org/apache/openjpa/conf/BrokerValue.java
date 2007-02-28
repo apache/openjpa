661f673890c076b8d6532d5f704434bc508a1fe5
Patrick Linskey <pcl@apache.org>
2007-02-28 19:52:24 +0000

    OPENJPA-160. Removed BrokerImpl test to make this work properly for subclasses. Since BrokerImpl is cloneable, all subtypes will also be cloneable, so we don't really need to test for anything.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openjpa/trunk@512934 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/conf/BrokerValue.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/conf/BrokerValue.java
@@ -30,20 +30,16 @@ public class BrokerValue
 
     public Object newInstance(String clsName, Class type, Configuration conf,
         boolean fatal) {
-        if (BrokerImpl.class.getName().equals(clsName)) {
-            // This is not synchronized. If there are concurrent invocations
-            // while _templateBroker is null, we'll just end up with extra
-            // template brokers, which will get safely garbage collected.
-            if (_templateBroker == null)
-                _templateBroker = (BrokerImpl) super.newInstance(
-                    clsName, type, conf, fatal);
-            try {
-                return _templateBroker.clone();
-            } catch (CloneNotSupportedException e) {
-                throw new InternalException(e);
-            }
-        } else {
-            return super.newInstance(clsName, type, conf, fatal);
+        // This is not synchronized. If there are concurrent invocations
+        // while _templateBroker is null, we'll just end up with extra
+        // template brokers, which will get safely garbage collected.
+        if (_templateBroker == null)
+            _templateBroker = (BrokerImpl) super.newInstance(clsName, type,
+                conf, fatal);
+        try {
+            return _templateBroker.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new InternalException(e);
         }
     }
 }
