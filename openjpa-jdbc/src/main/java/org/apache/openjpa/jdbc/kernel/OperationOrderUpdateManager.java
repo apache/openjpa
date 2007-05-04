ab72580c3eee5c9af24edf5e2e6de49c10a7c947
Patrick Linskey <pcl@apache.org>
2007-05-04 20:58:49 +0000

    OPENJPA-235. Reformatted code to meet OpenJPA conventions; widened some type arguments that seemed unnecessarily narrow.
    This passes all the OpenJPA tests in my environment, and the logic seems sound. I think that we could adjust the algorithm to require less collection copying, but I don't think that we should hold up the commit for that type of optimization.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/openjpa/trunk@535379 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/OperationOrderUpdateManager.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/OperationOrderUpdateManager.java
@@ -14,7 +14,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.openjpa.jdbc.kernel;
 
@@ -22,8 +22,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+import java.util.Map;
 
+import org.apache.openjpa.jdbc.schema.Column;
 import org.apache.openjpa.jdbc.schema.ForeignKey;
 import org.apache.openjpa.jdbc.sql.PrimaryRow;
 import org.apache.openjpa.jdbc.sql.Row;
@@ -34,7 +39,7 @@ import org.apache.openjpa.jdbc.sql.SQLExceptions;
 import org.apache.openjpa.kernel.OpenJPAStateManager;
 
 /**
- * Update manager that writes SQL in object-level operation order.
+ * Update manager that writes SQL in object-level operation order
  *
  * @author Abe White
  */
@@ -66,13 +71,20 @@ public class OperationOrderUpdateManager
 
         // now do any 'all row' updates, which typically null keys
         flush(rmimpl.getAllRowUpdates(), psMgr);
+        
+        // map statemanagers to primaryrows
+        Map smMap = mapStateManagers(rmimpl.getOrdered());
+        
+        // order rows to avoid constraint violations
+        List orderedRows = orderRows(rmimpl, smMap);
 
         // gather any updates we need to avoid fk constraints on deletes
         Collection constraintUpdates = null;
-        for (Iterator itr = rmimpl.getDeletes().iterator(); itr.hasNext();) {
+        for (Iterator itr = orderedRows.iterator(); itr.hasNext();) {
             try {
                 constraintUpdates = analyzeDeleteConstraints(rmimpl,
-                    (PrimaryRow) itr.next(), constraintUpdates);
+                    (PrimaryRow) itr.next(), constraintUpdates, smMap,
+                    orderedRows);
             } catch (SQLException se) {
                 exceps = addException(exceps, SQLExceptions.getStore
                     (se, dict));
@@ -82,17 +94,18 @@ public class OperationOrderUpdateManager
             flush(constraintUpdates, psMgr);
             constraintUpdates.clear();
         }
-
+        
         // flush primary rows in order
-        for (Iterator itr = rmimpl.getOrdered().iterator(); itr.hasNext();) {
+        for (Iterator itr = orderedRows.iterator(); itr.hasNext();) {
             try {
                 constraintUpdates = flushPrimaryRow(rmimpl, (PrimaryRow)
-                    itr.next(), psMgr, constraintUpdates);
+                    itr.next(), psMgr, constraintUpdates, smMap, orderedRows);
             } catch (SQLException se) {
                 exceps = addException(exceps, SQLExceptions.getStore
                     (se, dict));
             }
         }
+
         if (constraintUpdates != null)
             flush(constraintUpdates, psMgr);
 
@@ -107,13 +120,121 @@ public class OperationOrderUpdateManager
     }
 
     /**
+     * Reorders all rows provided by the specified RowManagerImpl such that
+     * no foreign key constraints are violated (assuming a proper schema).
+     * @param rmimpl RowManagerImpl
+     */
+    private List orderRows(RowManagerImpl rmimpl, Map smMap) {
+        List orderedRows = new ArrayList();
+        if (rmimpl.getOrdered().size() > 0) {
+            List inserts = new ArrayList(rmimpl.getInserts());
+            List updates = new ArrayList(rmimpl.getUpdates());
+            List deletes = new ArrayList(rmimpl.getDeletes());
+
+            orderedRows.addAll(orderRows(inserts, smMap));
+            orderedRows.addAll(updates);
+            orderedRows.addAll(orderRows(deletes, smMap));
+        }
+        return orderedRows;
+    }
+
+    private List orderRows(List unorderedList, Map smMap) {
+        List orderedList = new ArrayList();
+        // this iterates in a while loop instead of with an iterator to
+        // avoid ConcurrentModificationExceptions, as unorderedList is
+        // mutated in the orderRow() invocation.
+        while (!unorderedList.isEmpty()) {
+            PrimaryRow nextRow = (PrimaryRow) unorderedList.get(0);
+            orderRow(nextRow, unorderedList, orderedList, smMap, new Stack());
+        }
+        return orderedList;
+    }
+
+    private void orderRow(PrimaryRow currentRow, Collection unordered,
+        List orderedList, Map smMap, Stack visitedRows) {
+        if (orderedList.contains(currentRow)) {
+            return;
+        }
+
+        // a circular reference found which means there is a problem
+        // with the underlying database schema and/or class metadata
+        // definitions. nothing can be done here to correct the problem.
+        if (visitedRows.contains(currentRow)) {
+            orderedList.addAll(unordered);
+            unordered.clear();
+            return;
+        }
+
+        if (currentRow.getAction() == Row.ACTION_INSERT) {
+            ForeignKey[] fks = currentRow.getTable().getForeignKeys();
+            OpenJPAStateManager sm;
+            for (int i = 0; i < fks.length; i++) {
+                sm = currentRow.getForeignKeySet(fks[i]);
+                if (sm == null)
+                    continue;
+                // if the foreign key is new and it's primary key is
+                // auto assigned
+                PrimaryRow fkRow = (PrimaryRow) smMap.get(sm);
+                if (fkRow.getAction() == Row.ACTION_INSERT) {
+                    boolean nullable = true;
+                    Column[] columns = fks[i].getColumns();
+                    for (int j = 0; j < columns.length; j++) {
+                        if (columns[j].isNotNull()) {
+                            nullable = false;
+                            break;
+                        }
+                    }
+                    if (!nullable) {
+                        visitedRows.push(currentRow);
+                        PrimaryRow nextRow = (PrimaryRow) smMap.get(sm);
+                        orderRow(nextRow, unordered, orderedList, smMap,
+                            visitedRows);
+                        visitedRows.pop();
+                    }
+                }
+            }
+            if (!orderedList.contains(currentRow)) {
+                unordered.remove(currentRow);
+                orderedList.add(currentRow);
+            }
+        } else if (currentRow.getAction() == Row.ACTION_DELETE) {
+            ForeignKey[] fks = currentRow.getTable().getForeignKeys();
+            OpenJPAStateManager sm;
+            for (int i = 0; i < fks.length; i++) {
+                sm = currentRow.getForeignKeySet(fks[i]);
+                if (sm == null)
+                    continue;
+                PrimaryRow fkRow = (PrimaryRow) smMap.get(sm);
+                // if the foreign key is going to be deleted
+                if (!orderedList.contains(fkRow)
+                    && fkRow.getAction() == Row.ACTION_DELETE) {
+                    visitedRows.add(currentRow);
+                    orderRow(fkRow, unordered, orderedList, smMap, visitedRows);
+                    visitedRows.remove(currentRow);
+                }
+            }
+            unordered.remove(currentRow);
+            orderedList.add(0, currentRow);
+        }
+    }
+
+    private Map mapStateManagers(List rowList) {
+        Map smMap = new HashMap();
+        for (Iterator iter = rowList.iterator(); iter.hasNext();) {
+            PrimaryRow row = (PrimaryRow) iter.next();
+            smMap.put(row.getPrimaryKey(), row);
+        }
+        return smMap;
+    }
+
+    /**
      * Analyze the delete constraints on the given row, gathering necessary
      * updates to null fks before deleting.
      */
     private Collection analyzeDeleteConstraints(RowManagerImpl rowMgr,
-        PrimaryRow row, Collection updates)
+        PrimaryRow row, Collection updates, Map smMap, List orderedRows)
         throws SQLException {
-        if (!row.isValid())
+        if (!row.isValid() || row.getAction() != Row.ACTION_DELETE)
             return updates;
 
         ForeignKey[] fks = row.getTable().getForeignKeys();
@@ -126,6 +247,11 @@ public class OperationOrderUpdateManager
             if (sm == null)
                 sm = row.getForeignKeyWhere(fks[i]);
             if (sm == null)
+                continue;
+            PrimaryRow fkRow = (PrimaryRow) smMap.get(sm);
+            int fkIndex = orderedRows.indexOf(fkRow);
+            int rIndex = orderedRows.indexOf(row);
+            if (fkIndex > rIndex)
                 continue;
 
             // only need an update if we have an fk to a row that's being
@@ -152,7 +278,8 @@ public class OperationOrderUpdateManager
      * Flush the given row, creating deferred updates for dependencies.
      */
     private Collection flushPrimaryRow(RowManagerImpl rowMgr, PrimaryRow row,
-        PreparedStatementManager psMgr, Collection updates)
+        PreparedStatementManager psMgr, Collection updates, Map smMap,
+        List orderedRows)
         throws SQLException {
         if (!row.isValid())
             return updates;
@@ -170,6 +297,13 @@ public class OperationOrderUpdateManager
         for (int i = 0; i < fks.length; i++) {
             sm = row.getForeignKeySet(fks[i]);
             if (sm == null)
+                continue;
+
+            PrimaryRow fkRow = (PrimaryRow) smMap.get(sm);
+            int fkIndex = orderedRows.indexOf(fkRow);
+            int rIndex = orderedRows.indexOf(row);
+            // consider sm flushed, no need to defer
+            if (rIndex > fkIndex)
                 continue;
 
             // only need an update if we have an fk to a row that's being
