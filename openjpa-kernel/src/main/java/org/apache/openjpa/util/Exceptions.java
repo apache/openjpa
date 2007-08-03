a361f6a148d5d352f21e72c282052ac2e266805c
Marc Prud'hommeaux <mprudhom@apache.org>
2007-08-03 23:38:34 +0000

    Changed exception message to show the version id (e.g., 1.0.0-SNAPSHOT-r12345), rather than redundant version status string that had been output (e.g., 1.0.0-SNAPSHOT-SNAPSHOT).
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@562615 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/util/Exceptions.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/util/Exceptions.java
@@ -139,8 +139,8 @@ public class Exceptions {
         int type = e.getType();
         StringBuffer buf = new StringBuffer();
         buf.append("<").
-            append(OpenJPAVersion.VERSION_NUMBER).
-            append(OpenJPAVersion.RELEASE_STATUS).append(' ').
+            append(OpenJPAVersion.VERSION_ID).
+            append(' ').
             append(e.isFatal() ? "fatal " : "nonfatal ").
             append (type == ExceptionInfo.GENERAL ? "general error" :
                 type == ExceptionInfo.INTERNAL ? "internal error" :
