6687c01f114a9b878c79a83f24d04c14703d4090
Patrick Linskey <pcl@apache.org>
2008-01-31 08:56:46 +0000

    OPENJPA-258. Actually really fixing the issue this time.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.0.x@617073 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestMetaDataInheritanceComparator.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestMetaDataInheritanceComparator.java
@@ -20,16 +20,45 @@ package org.apache.openjpa.meta;
 
 import javax.persistence.EntityManagerFactory;
 
-import junit.framework.TestCase;
 import org.apache.openjpa.persistence.test.PersistenceTestCase;
-import org.apache.openjpa.persistence.test.SingleEMFTestCase;
 import org.apache.openjpa.persistence.JPAFacadeHelper;
 
 public class TestMetaDataInheritanceComparator extends PersistenceTestCase {
 
-    public void testMetaDataInheritanceComparator() {
+    public void testInheritanceComparatorWithoutBase() {
+        inheritanceComparatorHelper(false);
+    }
+
+    public void testInheritanceComparatorWithBase() {
+        inheritanceComparatorHelper(true);
+    }
+
+    private void inheritanceComparatorHelper(boolean base) {
+        InheritanceComparator comp = new InheritanceComparator();
+        if (base)
+            comp.setBase(AbstractThing.class);
+
+        assertEquals(-1, comp.compare(A.class, B.class));
+        assertEquals(-1, comp.compare(B.class, C.class));
+        assertTrue(comp.compare(A.class, C.class) < 0);
+
+        assertEquals(-1, comp.compare(AbstractThing.class, A.class));
+        assertEquals(-1, comp.compare(AbstractThing.class, B.class));
+        assertTrue(comp.compare(AbstractThing.class, C.class) < 0);
+    }
+
+    public void testMetaDataInheritanceComparatorWithoutBase() {
+        metaDataInheritanceComparatorHelper(false);
+    }
+
+    public void testMetaDataInheritanceComparatorWithBase() {
+        metaDataInheritanceComparatorHelper(true);
+    }
+
+    private void metaDataInheritanceComparatorHelper(boolean base) {
         InheritanceComparator comp = new MetaDataInheritanceComparator();
-        comp.setBase(AbstractThing.class);
+        if (base)
+            comp.setBase(AbstractThing.class);
 
         EntityManagerFactory emf = createEMF(A.class, B.class, C.class,
             AbstractThing.class);
@@ -37,18 +66,29 @@ public class TestMetaDataInheritanceComparator extends PersistenceTestCase {
         ClassMetaData a = JPAFacadeHelper.getMetaData(emf, A.class);
         ClassMetaData b = JPAFacadeHelper.getMetaData(emf, B.class);
         ClassMetaData c = JPAFacadeHelper.getMetaData(emf, C.class);
+        ClassMetaData at = JPAFacadeHelper.getMetaData(emf,
+            AbstractThing.class);
+
+        emf.close();
 
         assertEquals(-1, comp.compare(a, b));
         assertEquals(-1, comp.compare(b, c));
-        assertEquals(-1, comp.compare(a, c));
+        assertTrue(comp.compare(a, c) < 0);
+
+        assertEquals(1, comp.compare(b, a));
+        assertEquals(1, comp.compare(c, b));
+        assertTrue(comp.compare(c, a) > 0);
+
+        assertEquals(-1, comp.compare(at, a));
+        assertEquals(-1, comp.compare(at, b));
+        assertEquals(-1, comp.compare(at, c));
     }
 
-    public void testInheritanceComparator() {
-        InheritanceComparator comp = new InheritanceComparator();
-        comp.setBase(AbstractThing.class);
-
-        assertEquals(-1, comp.compare(A.class, B.class));
-        assertEquals(-1, comp.compare(B.class, C.class));
-        assertEquals(-1, comp.compare(A.class, C.class));
+    public void testEndToEnd() {
+        // make sure we can get things fully instantiated
+        EntityManagerFactory emf = createEMF(A.class, B.class, C.class,
+            AbstractThing.class);
+        emf.createEntityManager().close();
+        emf.close();
     }
 }
