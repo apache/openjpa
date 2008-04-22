44e06d03a31c2a2a4f08fbccc6b3021b230ad427
Patrick Linskey <pcl@apache.org>
2008-04-22 15:46:58 +0000

    OPENJPA-536. Committing on behalf of Amy Yang.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.1.x@650559 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestGetMetaData.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestGetMetaData.java
@@ -29,10 +29,7 @@ public class TestGetMetaData extends SingleEMFTestCase {
     }
 
     public void testGetMetaData() {
-        // pcl: 10 April 2008: disabling temporarily as this is causing
-        // integration problems. A more complete fix will be forthcoming.
-        // See OPENJPA-536. Also disabled code in MetaDataRepository.
-        // assertNotNull(JPAFacadeHelper.getMetaData(emf, Item.class));
-        // assertNotNull(JPAFacadeHelper.getMetaData(emf, Person.class));
+        assertNotNull(JPAFacadeHelper.getMetaData(emf, Item.class));
+        assertNotNull(JPAFacadeHelper.getMetaData(emf, Person.class));
     }
 }
