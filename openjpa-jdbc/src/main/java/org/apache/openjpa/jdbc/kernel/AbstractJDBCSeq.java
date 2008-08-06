62ff7fa9c64b1431933fb0dd07a863c0d32be240
Michael Dick <mikedd@apache.org>
2008-08-06 15:42:10 +0000

    OPENJPA-676 correct extra bracket.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.2.x@683303 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/AbstractJDBCSeq.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/kernel/AbstractJDBCSeq.java
@@ -190,7 +190,6 @@ public abstract class AbstractJDBCSeq
             	catch(SQLException se) { 
             		throw SQLExceptions.getStore(se);
             	}
-            }
             return;
         }
         else {
