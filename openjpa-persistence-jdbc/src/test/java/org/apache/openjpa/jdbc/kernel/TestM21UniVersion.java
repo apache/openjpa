b8ec28b38a20339c5edc4e6522a4570b2b9d570a
Michael Dick <mikedd@apache.org>
2009-01-13 22:05:07 +0000

    OPENJPA-843 refactor testcase so that it runs during a normal maven build.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@734253 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/jdbc/kernel/TestM21UniVersion.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/jdbc/kernel/TestM21UniVersion.java
@@ -18,14 +18,11 @@
  */
 package org.apache.openjpa.jdbc.kernel;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 import javax.persistence.EntityManager;
 
 import org.apache.openjpa.persistence.test.SingleEMFTestCase;
 
-public class M2MBiVersionTest extends SingleEMFTestCase {
+public class TestM21UniVersion extends SingleEMFTestCase {
     public static String SALESID = "SALES";
     public static String MARKETINGID = "MARKETING";
     
@@ -33,10 +30,11 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
     public static String EMPLOYEE2ID = "EMPLOYEE2";
     public static String EMPLOYEE3ID = "EMPLOYEE3";
     
+    
     public void setUp() {
         setUp(
-                M2MBiDepartment.class, 
-                M2MBiEmployee.class,
+                M21UniDepartment.class, 
+                M21UniEmployee.class,
                 CLEAR_TABLES);        
         
         createEntities();        
@@ -46,26 +44,23 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
         EntityManager em = emf.createEntityManager();
         
         em.getTransaction().begin();
-        M2MBiDepartment sales = new M2MBiDepartment();
+        M21UniDepartment sales = new M21UniDepartment();
         sales.setDeptid(SALESID);        
         sales.setName("SALES");
         sales.setCostCode("1000");
-        M2MBiDepartment marketing = new M2MBiDepartment();
+        M21UniDepartment marketing = new M21UniDepartment();
         marketing.setDeptid(MARKETINGID);        
         marketing.setName("marketing");
         marketing.setCostCode("3000");        
         
-        M2MBiEmployee e1 = new M2MBiEmployee();
-        M2MBiEmployee e2 = new M2MBiEmployee();
+        M21UniEmployee e1 = new M21UniEmployee();
+        M21UniEmployee e2 = new M21UniEmployee();
         e1.setEmpid(EMPLOYEE1ID);
         e1.setName("Gilgamesh_1");
         e2.setEmpid(EMPLOYEE2ID);
         e2.setName("Enkidu_1");
-        
-        e1.getDepartments().add(sales);
-        e2.getDepartments().add(sales);
-        sales.getEmployees().add(e1);
-        sales.getEmployees().add(e2);
+        e1.setDepartment(sales);
+        e2.setDepartment(sales);
         
         em.persist(e1);
         em.persist(e2);
@@ -78,25 +73,28 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
     }
     
     public void testNonRelationalFieldInverseSideVersionUpdate() {
-        EntityManager em = emf.createEntityManager();
+        // Change only non-relation fields on Department.
+        // Version number of Department should be updated.
+        // Version numbers of Employee should not be updated.
         
-        M2MBiDepartment sales = em.find(M2MBiDepartment.class, SALESID);
-        M2MBiEmployee e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        M2MBiEmployee e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        EntityManager em = emf.createEntityManager();
+        M21UniDepartment sales = em.find(M21UniDepartment.class, SALESID);
+        M21UniEmployee e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        M21UniEmployee e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPre = sales.getVersion();
         int e1VersionPre = e1.getVersion();
         int e2VersionPre = e2.getVersion();
         
-        em.getTransaction().begin();        
+        em.getTransaction().begin();
         sales.setCostCode("1001");
         em.getTransaction().commit();
         em.close();
         
         em = emf.createEntityManager();
-        sales = em.find(M2MBiDepartment.class, SALESID);
-        e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        sales = em.find(M21UniDepartment.class, SALESID);
+        e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPost = sales.getVersion();
         int e1VersionPost = e1.getVersion();
@@ -105,18 +103,18 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
         
         assertEquals(salesVersionPost, salesVersionPre + 1);
         assertEquals(e1VersionPost, e1VersionPre);
-        assertEquals(e2VersionPost, e2VersionPre);        
-    
+        assertEquals(e2VersionPost, e2VersionPre);
     }
-    
+
+
     public void testNonRelationalFieldOwnerSideVersionUpdate() {
         // Change only non-relation fields on Employee.
         // Version number of Employee should be updated.
         // Version number of Department should not change.
         EntityManager em = emf.createEntityManager();
-        M2MBiDepartment sales = em.find(M2MBiDepartment.class, SALESID);
-        M2MBiEmployee e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        M2MBiEmployee e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        M21UniDepartment sales = em.find(M21UniDepartment.class, SALESID);
+        M21UniEmployee e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        M21UniEmployee e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPre = sales.getVersion();
         int e1VersionPre = e1.getVersion();
@@ -129,9 +127,9 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
         em.close();
         
         em = emf.createEntityManager();
-        sales = em.find(M2MBiDepartment.class, SALESID);
-        e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        sales = em.find(M21UniDepartment.class, SALESID);
+        e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPost = sales.getVersion();
         int e1VersionPost = e1.getVersion();
@@ -142,75 +140,48 @@ public class M2MBiVersionTest extends SingleEMFTestCase {
         assertEquals(e1VersionPost, e1VersionPre + 1);
         assertEquals(e2VersionPost, e2VersionPre + 1);        
     }
-
     
-    public void testRelationalFieldBothSidesVersionUpdate() {
-        // Move Employee from old Department to new Department.
-        // Update both sides of the relationship.        
-        // Since there is a bidirectional ManyToMany relationship 
-        // from  Employee to Department, Employee version should
-        // be updated. Since neither the new nor the old Departments
-        // are owners of the reassigned Employee, the Department 
-        // versions should remain the same.
+    public void testRelationalFieldOwnerSideVersionUpdate() {
+        // Assign employees to a new Department. 
+        // Since there is a unidirectional ManyToOne relationship 
+        // from  Employee to Department, only the Employee
+        // version should be updated. Department version
+        // should remain the same as before.
         
         EntityManager em = emf.createEntityManager();
-        M2MBiDepartment sales = em.find(M2MBiDepartment.class, SALESID);
-        M2MBiDepartment marketing = em.find(M2MBiDepartment.class, MARKETINGID);
-        M2MBiEmployee e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        M2MBiEmployee e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        M21UniDepartment sales = em.find(M21UniDepartment.class, SALESID);
+        M21UniDepartment marketing = em.find(M21UniDepartment.class, MARKETINGID);
+        M21UniEmployee e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        M21UniEmployee e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPre = sales.getVersion();
         int marketingVersionPre = marketing.getVersion();
         int e1VersionPre = e1.getVersion();
         int e2VersionPre = e2.getVersion();
-        
-        em.getTransaction().begin();
-        // Remove sales from e1
-        Collection<M2MBiDepartment> e1Departments = e1.getDepartments();
-        for (Iterator<M2MBiDepartment> dIterator = e1Departments.iterator(); dIterator.hasNext();) {
-            M2MBiDepartment d = dIterator.next();
-            if (SALESID.equals(d.getDeptid())) {
-                dIterator.remove();
-                break;
-            }
-        }
-        // remove e1 from sales
-        Collection<M2MBiEmployee> salesEmployees = sales.getEmployees();
-        for (Iterator<M2MBiEmployee> eIterator = salesEmployees.iterator(); eIterator.hasNext();) {
-            M2MBiEmployee e = eIterator.next();
-            if (EMPLOYEE1ID.equals(e.getEmpid())) {
-                eIterator.remove();
-                break;
-            }
-        }
-        
-        // Add marketing to e1
-        e1.getDepartments().add(marketing);
-        // Add e1 to marketing
-        marketing.getEmployees().add(e1);
-        
+                
+        em.getTransaction().begin();        
+        e1.setDepartment(marketing);
+        // Don't update e2, so we can check for unchanged
+        // version number for e2.        
         em.getTransaction().commit();
         em.close();
         
         em = emf.createEntityManager();
-        sales = em.find(M2MBiDepartment.class, SALESID);
-        marketing = em.find(M2MBiDepartment.class, MARKETINGID);
-        e1 = em.find(M2MBiEmployee.class, EMPLOYEE1ID);
-        e2 = em.find(M2MBiEmployee.class, EMPLOYEE2ID);
+        sales = em.find(M21UniDepartment.class, SALESID);
+        marketing = em.find(M21UniDepartment.class, MARKETINGID);
+        e1 = em.find(M21UniEmployee.class, EMPLOYEE1ID);
+        e2 = em.find(M21UniEmployee.class, EMPLOYEE2ID);
         
         int salesVersionPost = sales.getVersion();
         int marketingVersionPost = marketing.getVersion();
         int e1VersionPost = e1.getVersion();
         int e2VersionPost = e2.getVersion();
+                
         em.close();
         
-        // Since Department is inverse side, there should
-        // be no version update when its employees are moved.
         assertEquals(salesVersionPost, salesVersionPre);
         assertEquals(marketingVersionPost, marketingVersionPre);
-        // Employee e1 was moved to marketing.
         assertEquals(e1VersionPost, e1VersionPre + 1);
-        // Employee e2 was unchanged.
         assertEquals(e2VersionPost, e2VersionPre);        
     }
 }
