c5736acdf423b312fde13a86d34ec4932e22acc9
Michael Dick <mikedd@apache.org>
2009-01-22 19:16:37 +0000

    OPENJPA-864 do not remove table aliases for databases which use JoinSyntaxes.SYNTAX_DATABASE (no joins).
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@736748 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/SelectImpl.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/SelectImpl.java
@@ -581,39 +581,41 @@ public class SelectImpl
 
         if (_parent._aliases.size() <= 1)
             return;
- 
-        // resolve aliases for subselect from parent
-        Set<Map.Entry> entries = _parent.getAliases().entrySet();
-        for (Map.Entry entry : entries) {
-            Object key = entry.getKey();
-            Integer alias = (Integer) entry.getValue();
-            if (key.toString().indexOf(_subPath) != -1 ||
-                _parent.findTableAlias(alias) == false) {
-                if (_aliases == null)
-                    _aliases = new HashMap();
-                _aliases.put(key, alias);
-
-                Object tableString = _parent.getTables().get(alias);
-                if (_tables == null)
-                    _tables = new TreeMap();
-                _tables.put(alias, tableString);
-                
-                _removedAliasFromParent.set(alias.intValue());
-            }
-        }
-        
-        if (_aliases != null) {
-            // aliases moved into subselect should be removed from parent
-            entries = _aliases.entrySet();
+        // Do not remove aliases for databases that use SYNTAX_DATABASE (oracle)
+        if(_parent._joinSyntax != JoinSyntaxes.SYNTAX_DATABASE) {
+            // resolve aliases for subselect from parent
+            Set<Map.Entry> entries = _parent.getAliases().entrySet();
             for (Map.Entry entry : entries) {
                 Object key = entry.getKey();
                 Integer alias = (Integer) entry.getValue();
                 if (key.toString().indexOf(_subPath) != -1 ||
                     _parent.findTableAlias(alias) == false) {
-                    _parent.removeAlias(key);
-
+                    if (_aliases == null)
+                        _aliases = new HashMap();
+                    _aliases.put(key, alias);
+    
                     Object tableString = _parent.getTables().get(alias);
-                    _parent.removeTable(alias);
+                    if (_tables == null)
+                        _tables = new TreeMap();
+                    _tables.put(alias, tableString);
+                    
+                    _removedAliasFromParent.set(alias.intValue());
+                }
+            }
+            
+            if (_aliases != null) {
+                // aliases moved into subselect should be removed from parent
+                entries = _aliases.entrySet();
+                for (Map.Entry entry : entries) {
+                    Object key = entry.getKey();
+                    Integer alias = (Integer) entry.getValue();
+                    if (key.toString().indexOf(_subPath) != -1 ||
+                        _parent.findTableAlias(alias) == false) {
+                        _parent.removeAlias(key);
+    
+                        Object tableString = _parent.getTables().get(alias);
+                        _parent.removeTable(alias);
+                    }
                 }
             }
         }
