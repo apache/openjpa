aba32ee812acbf8be51a574fd89fbfbfdbf7748b
Michael Dick <mikedd@apache.org>
2009-07-10 02:44:49 +0000

    OPENJPA-732: Prevent duplicate events from being fired (stored in Sets instead of Lists) Changes originally made by Albert Lee were merged to 1.2.x. Submitted by: B. J. Reed. 	modified:   openjpa-kernel/src/main/java/org/apache/openjpa/kernel/BrokerImpl.java 	new file:   openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/callbacks/Message.java 	new file:   openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/callbacks/MessageListenerImpl.java 	new file:   openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/callbacks/TestMessageListeners.java 	modified:   openjpa-persistence-jdbc/src/test/resources/META-INF/listener-orm.xml 	modified:   openjpa-persistence-jdbc/src/test/resources/META-INF/persistence.xml
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.2.x@792787 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/kernel/BrokerImpl.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/kernel/BrokerImpl.java
@@ -32,6 +32,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -1521,7 +1522,7 @@ public class BrokerImpl
                 _savepoints = new LinkedMap();
             } else {
                 if (_savepointCache == null)
-                    save.save(Collections.EMPTY_LIST);
+                    save.save(Collections.EMPTY_SET);
                 else {
                     save.save(_savepointCache);
                     _savepointCache.clear();
@@ -2189,7 +2190,7 @@ public class BrokerImpl
         if (hasTransactionalObjects())
             transStates = _transCache;
         else
-            transStates = Collections.EMPTY_LIST;
+            transStates = Collections.EMPTY_SET;
 
         // fire after rollback/commit event
         Collection mobjs = null;
@@ -2264,7 +2265,7 @@ public class BrokerImpl
 
         // now clear trans cache; keep cleared version rather than
         // null to avoid having to re-create the set later; more efficient
-        if (transStates != Collections.EMPTY_LIST) {
+        if (transStates != Collections.EMPTY_SET) {
             _transCache = (TransactionalCache) transStates;
             _transCache.clear();
         }
@@ -3309,7 +3310,7 @@ public class BrokerImpl
             Object obj;
             StateManagerImpl sm;
             ClassMetaData meta;
-            Collection sms = new ArrayList(objs.size());
+            Collection sms = new LinkedHashSet(objs.size());
             List exceps = null;
             for (Iterator itr = objs.iterator(); itr.hasNext();) {
                 obj = itr.next();
@@ -3636,7 +3637,7 @@ public class BrokerImpl
         try {
             assertActiveTransaction();
 
-            Collection sms = new ArrayList(objs.size());
+            Collection sms = new LinkedHashSet(objs.size());
             Object obj;
             StateManagerImpl sm;
             for (Iterator itr = objs.iterator(); itr.hasNext();) {
@@ -3783,7 +3784,7 @@ public class BrokerImpl
      */
     protected Collection getTransactionalStates() {
         if (!hasTransactionalObjects())
-            return Collections.EMPTY_LIST;
+            return Collections.EMPTY_SET;
         return _transCache.copy();
     }
 
@@ -3802,7 +3803,7 @@ public class BrokerImpl
      */
     protected Collection getDirtyStates() {
         if (!hasTransactionalObjects())
-            return Collections.EMPTY_LIST;
+            return Collections.EMPTY_SET;
 
         return _transCache.copyDirty();
     }
@@ -3813,8 +3814,8 @@ public class BrokerImpl
      */
     protected Collection getPendingTransactionalStates() {
         if (_pending == null)
-            return Collections.EMPTY_LIST;
-        return new ArrayList(_pending);
+            return Collections.EMPTY_SET;
+        return new LinkedHashSet(_pending);
     }
 
     /**
@@ -4035,19 +4036,19 @@ public class BrokerImpl
 
     public Collection getPersistedTypes() {
         if (_persistedClss == null || _persistedClss.isEmpty())
-            return Collections.EMPTY_LIST;
+            return Collections.EMPTY_SET;
         return Collections.unmodifiableCollection(_persistedClss);
     }
 
     public Collection getUpdatedTypes() {
         if (_updatedClss == null || _updatedClss.isEmpty())
-            return Collections.EMPTY_LIST;
+            return Collections.EMPTY_SET;
         return Collections.unmodifiableCollection(_updatedClss);
     }
 
     public Collection getDeletedTypes() {
         if (_deletedClss == null || _deletedClss.isEmpty())
-            return Collections.EMPTY_LIST;
+            return Collections.EMPTY_SET;
         return Collections.unmodifiableCollection(_deletedClss);
     }
 
@@ -4502,12 +4503,12 @@ public class BrokerImpl
          */
         public Collection copy() {
             if (isEmpty())
-                return Collections.EMPTY_LIST;
+                return Collections.EMPTY_SET;
 
             // size may not be entirely accurate due to refs expiring, so
             // manually copy each object; doesn't matter this way if size too
             // big by some
-            List copy = new ArrayList(size());
+            Set copy = new LinkedHashSet(size());
             if (_dirty != null)
                 for (Iterator itr = _dirty.iterator(); itr.hasNext();)
                     copy.add(itr.next());
@@ -4522,8 +4523,8 @@ public class BrokerImpl
          */
         public Collection copyDirty() {
             if (_dirty == null || _dirty.isEmpty())
-                return Collections.EMPTY_LIST;
-            return new ArrayList(_dirty);
+                return Collections.EMPTY_SET;
+            return new LinkedHashSet(_dirty);
         }
 
         /**
