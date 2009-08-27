8e86ff09aae9cc6083d240949917f3128d7b713a
Michael Dick <mikedd@apache.org>
2009-08-27 13:34:27 +0000

    OPENJPA-386: Handle abstract and non abstract MappedSuperClasses identically. Submitted By: Jody Grassel
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.3.x@808413 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/meta/ClassMetaData.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/meta/ClassMetaData.java
@@ -1923,7 +1923,8 @@ public class ClassMetaData
     private boolean hasConcretePCSuperclass() {
         if (_super == null)
             return false;
-        if (!Modifier.isAbstract(_super.getModifiers()))
+        if (!Modifier.isAbstract(_super.getModifiers()) && 
+        		(!getPCSuperclassMetaData().isAbstract()))
             return true;
         return getPCSuperclassMetaData().hasConcretePCSuperclass();
     }
