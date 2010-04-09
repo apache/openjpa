9b445c7bc2600232b537bcb827e7d397abc60757
Donald Woods <dwoods@apache.org>
2010-04-09 00:35:34 +0000

    OPENJPA-1616 Fix TestTimeoutException tests for Oracle and re-enable for DB2
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/2.0.x@932195 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/query/TestTimeoutException.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/query/TestTimeoutException.java
@@ -43,9 +43,6 @@ public class TestTimeoutException extends SingleEMFTestCase {
     private final Class<?> entityClass = PObject.class;
 
     public void setUp() {
-        // TODO - Hack until OPENJPA-1594 is addressed
-        setUnsupportedDatabases(OracleDictionary.class, DB2Dictionary.class);
-        
         // Disable tests for any DB that has supportsSelectForUpdate==false, like HSQLDictionary
         OpenJPAEntityManagerFactorySPI tempEMF = emf;
         if (tempEMF == null) {
