dab906385e2723423d5b9c243457ed2befc034cb
Donald Woods <dwoods@apache.org>
2010-04-10 17:21:48 +0000

    OPENJPA-1569 remove commented code
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@932766 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/meta/JavaTypes.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/meta/JavaTypes.java
@@ -156,7 +156,6 @@ public class JavaTypes {
             return CALENDAR;
 
         if (type.isInterface()) {
-            //if (type == Serializable.class)
             if (Serializable.class.isAssignableFrom(type))
                 return OBJECT;
             return PC_UNTYPED;
