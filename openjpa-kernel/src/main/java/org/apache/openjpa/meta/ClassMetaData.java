fda7059a8854855529542a8ac8c7809c573028e2
Michael Dick <mikedd@apache.org>
2010-08-16 14:10:11 +0000

    OPENJPA-1424: Do not lookup extraFieldData with another mapping's field. Submitted By: Heath Thomann, Daryl Stultz, Pinaki Poddar
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.3.x@985941 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/meta/ClassMetaData.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/meta/ClassMetaData.java
@@ -866,7 +866,11 @@ public class ClassMetaData
      * @see #getExtraFieldDataLength
      */
     public int getExtraFieldDataIndex(int field) {
-        return getExtraFieldDataTable()[field];
+        int[] array = getExtraFieldDataTable();
+        if (field < 0 || field >= array.length) {
+            return -1;
+        }
+        return array[field];
     }
 
     /**
