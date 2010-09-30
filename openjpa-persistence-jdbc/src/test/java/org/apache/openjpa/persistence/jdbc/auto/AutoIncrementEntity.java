1f64b37ec51ac86c953aceb5288bbe2a2237e2a2
Richard G. Curtis <curtisr7@apache.org>
2010-09-30 21:29:00 +0000

    Misc test update.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1003292 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/jdbc/auto/AutoIncrementEntity.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/jdbc/auto/AutoIncrementEntity.java
@@ -22,6 +22,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class AutoIncrementEntity {
@@ -29,6 +30,9 @@ public class AutoIncrementEntity {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     public int id;
 
+    @Version
+    int version;
+    
     public String somethingElse;
     
     public int getId(){
