11969d128df2ed5ca5e45738a9430b3cf2669838
Donald Woods <dwoods@apache.org>
2010-10-08 15:39:52 +0000

    make sure tests cleanup EMs and EMFs
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1005863 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestMetaDataInheritanceComparator.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/meta/TestMetaDataInheritanceComparator.java
@@ -20,16 +20,18 @@ package org.apache.openjpa.meta;
 
 import javax.persistence.EntityManagerFactory;
 
-import org.apache.openjpa.persistence.test.AbstractCachedEMFTestCase;
-import org.apache.openjpa.persistence.test.PersistenceTestCase;
+import org.apache.openjpa.persistence.test.SingleEMFTestCase;
 import org.apache.openjpa.persistence.JPAFacadeHelper;
 
-public class TestMetaDataInheritanceComparator extends AbstractCachedEMFTestCase {
+public class TestMetaDataInheritanceComparator extends SingleEMFTestCase {
 
     public void testInheritanceComparatorWithBase() {
         inheritanceComparatorHelper(true);
     }
 
+    public void setUp() {
+        setUp(A.class, B.class, C.class, AbstractThing.class);
+    }
     private void inheritanceComparatorHelper(boolean base) {
         InheritanceComparator comp = new InheritanceComparator();
         if (base)
@@ -53,16 +55,11 @@ public class TestMetaDataInheritanceComparator extends AbstractCachedEMFTestCase
         if (base)
             comp.setBase(AbstractThing.class);
 
-        EntityManagerFactory emf = createEMF(A.class, B.class, C.class,
-            AbstractThing.class);
-
         ClassMetaData a = JPAFacadeHelper.getMetaData(emf, A.class);
         ClassMetaData b = JPAFacadeHelper.getMetaData(emf, B.class);
         ClassMetaData c = JPAFacadeHelper.getMetaData(emf, C.class);
         ClassMetaData at = JPAFacadeHelper.getMetaData(emf,
             AbstractThing.class);
-
-        emf.close();
 
         assertEquals(-1, comp.compare(a, b));
         assertEquals(-1, comp.compare(b, c));
@@ -79,9 +76,6 @@ public class TestMetaDataInheritanceComparator extends AbstractCachedEMFTestCase
 
     public void testEndToEnd() {
         // make sure we can get things fully instantiated
-        EntityManagerFactory emf = createEMF(A.class, B.class, C.class,
-            AbstractThing.class);
         emf.createEntityManager().close();
-        emf.close();
     }
 }
