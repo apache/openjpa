1edd0d97c1fd83c9cd94d12a80b8195871914ff7
Donald Woods <dwoods@apache.org>
2010-10-13 00:09:07 +0000

    missed a testcase update in r1021956
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1021957 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/criteria/results/TestMultiselect.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/criteria/results/TestMultiselect.java
@@ -43,7 +43,7 @@ public class TestMultiselect extends CriteriaTest {
     private static boolean initialized = false;
     
     @Override
-    public void setUp() {
+    public void setUp() throws Exception {
         super.setUp();
         if (!initialized) {
             clearData();
