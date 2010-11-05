6d423c5b6b04f683b545d2890f0d74b785f44be5
Michael Dick <mikedd@apache.org>
2010-11-05 15:57:26 +0000

    OPENJPA-1874: handle Oracle XMLType columns with @Lob annotation
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1031648 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/schema/Column.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/schema/Column.java
@@ -717,10 +717,13 @@ public class Column
                     case Types.CHAR:
                     case Types.LONGVARCHAR:
                     case Types.VARCHAR:
+                    case Types.CLOB:
+                    case Types.BLOB:
                         return true;
                      default:
                          return false;
                 }
+                
             default:
                 return type == getType();
         }
