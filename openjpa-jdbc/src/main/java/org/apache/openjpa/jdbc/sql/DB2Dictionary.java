b44614d94b13f90d879aff00d0c8bd62f368529f
Michael Dick <mikedd@apache.org>
2011-02-21 16:06:05 +0000

    OPENJPA-1939: Increase index length for DB2 and testcase workaround for OPENJPA-1948
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1073042 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/DB2Dictionary.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/DB2Dictionary.java
@@ -118,7 +118,7 @@ public class DB2Dictionary
         }));
         systemSchemas = "SYSCAT,SYSIBM,SYSSTAT,SYSIBMADM,SYSTOOLS";
         maxConstraintNameLength = 18;
-        maxIndexNameLength = 18;
+        maxIndexNameLength = 128;
         maxColumnNameLength = 30;
         supportsDeferredConstraints = false;
         supportsDefaultDeleteAction = false;
@@ -345,7 +345,6 @@ public class DB2Dictionary
             forUpdateClause = "WITH RR USE AND KEEP UPDATE LOCKS";
             
             if (maj >=9) { 
-                maxIndexNameLength=128;
                 supportsXMLColumn = true;
             }
         }
