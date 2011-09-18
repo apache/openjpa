10a1d43a1c01a2742640159380cd8812ff85bdea
Ravi Prakash Palacherla <rpalache@apache.org>
2011-09-18 05:34:46 +0000

    OPENJPA-677: Make a judgement whether reading discrimnator value from the result makes sense in a context. Bakporting Pinaki's fix to 1.1.x
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/1.1.x@1172200 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/meta/ClassMapping.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/meta/ClassMapping.java
@@ -171,6 +171,7 @@ public class ClassMapping
         FieldMapping fm;
         Joinable join;
         int pkIdx;
+        boolean canReadDiscriminator = true;
         for (int i = 0; i < pks.length; i++) {
             // we know that all pk column join mappings use primary key fields,
             // cause this mapping uses the oid as its primary key (we recursed
@@ -179,6 +180,7 @@ public class ClassMapping
             fm = getFieldMapping(join.getFieldIndex());
             pkIdx = fm.getPrimaryKeyIndex();
 
+            canReadDiscriminator &= isSelfReference(fk, join.getColumns());
             // could have already set value with previous multi-column joinable
             if (vals[pkIdx] == null) {
                 res.startDataRequest(fm);
@@ -193,8 +195,12 @@ public class ClassMapping
         // the oid data is loaded by the base type, but if discriminator data
         // is present, make sure to use it to construct the actual oid instance
         // so that we get the correct app id class, etc
+        // Discriminator refers to the row but the vals[] may hold data that
+        // refer to another row. Then there is little point reading the disc
+        // value
+
         ClassMapping dcls = cls;
-        if (subs) {
+        if (subs && canReadDiscriminator) {
             res.startDataRequest(cls.getDiscriminator());
             try {
                 Class dtype = cls.getDiscriminator().getClass(store, cls, res);
@@ -212,6 +218,15 @@ public class ClassMapping
                 subs);
         }
         return oid;
+    }
+
+    boolean isSelfReference(ForeignKey fk, Column[] cols) {
+        if (fk == null)
+            return true;
+        for (Column col : cols)
+            if (fk.getColumn(col) != col)
+                return false;
+        return true;
     }
 
     /**
