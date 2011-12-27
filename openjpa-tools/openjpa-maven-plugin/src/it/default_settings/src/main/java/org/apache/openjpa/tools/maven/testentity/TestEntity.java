apache/openjpa

commit d20b9a95117482a811c90df92c740b004438ea54

OPENJPA-1934: Cleanup compiler warnings.
git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1091279 13f79535-47bb-0310-9956-ffa450edef68

openjpa-tools/openjpa-maven-plugin/src/main/java/org/apache/openjpa/tools/maven/AbstractOpenJpaMappingToolMojo.java
@@ -19,10 +19,15 @@
 package org.apache.openjpa.tools.maven;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.persistence.Entity;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.plexus.util.FileUtils;
-
 import org.apache.openjpa.enhance.PersistenceCapable;
 import org.apache.openjpa.jdbc.conf.JDBCConfiguration;
 import org.apache.openjpa.jdbc.conf.JDBCConfigurationImpl;
@@ -31,14 +36,7 @@ import org.apache.openjpa.lib.conf.Configurations;
 import org.apache.openjpa.lib.meta.ClassArgParser;
 import org.apache.openjpa.lib.util.Options;
 import org.apache.openjpa.meta.MetaDataRepository;
-
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.persistence.Entity;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * Processes Application model classes and generate the DDL by running the 
@@ -78,7 +76,7 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
             FileUtils.mkdir(getEntityClasses().getAbsolutePath());
         }
 
-        List entities = findEntityClassFiles();
+        List<File> entities = findEntityClassFiles();
 
         mappingTool(entities);
     }
@@ -90,7 +88,7 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
      * @param files class file resources to map.
      * @throws MojoExecutionException if the MappingTool detected an error
      */
-    private void mappingTool(List files) throws MojoExecutionException {
+    private void mappingTool(List<File> files) throws MojoExecutionException {
         extendRealmClasspath();
 
         Options opts = getOptions();
@@ -126,23 +124,20 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
      * @param files List with classPath Files; non persistence classes will be removed
      * @param opts  filled configuration Options
      */
-    private void filterPersistenceCapable(List files, Options opts) {
+    private void filterPersistenceCapable(List<File> files, Options opts) {
         JDBCConfiguration conf = new JDBCConfigurationImpl();
         Configurations.populateConfiguration(conf, opts);
         MetaDataRepository repo = conf.newMetaDataRepositoryInstance();
         ClassArgParser cap = repo.getMetaDataFactory().newClassArgParser();
 
-        Iterator fileIt = files.iterator();
-        while (fileIt.hasNext()) {
-            File classPath = (File) fileIt.next();
-
-            Class[] classes = cap.parseTypes(classPath.getAbsolutePath());
+        for(File classPath : files) { 
+            Class<?>[] classes = cap.parseTypes(classPath.getAbsolutePath());
 
             if (classes == null) {
                 getLog().info("Found no classes for " + classPath.getAbsolutePath());
             } else {
                 for (int i = 0; i < classes.length; i++) {
-                    Class cls = classes[i];
+                    Class<?> cls = classes[i];
 
                     if (cls.getAnnotation(Entity.class) != null) {
                         getLog().debug("Found @Entity in class " + classPath);
@@ -151,7 +146,6 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
                                 + PersistenceCapable.class.getName());
                     } else {
                         getLog().debug("Removing non-entity class " + classPath);
-                        fileIt.remove();
                     }
                 }
             }
@@ -163,9 +157,9 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
      * @param cls the Class to check
      * @return <code>true</code> if the given Class cls implements the interface {@link PersistenceCapable}
      */
-    private boolean implementsPersistenceCapable(Class cls) {
+    private boolean implementsPersistenceCapable(Class<?> cls) {
         boolean isPersistenceCapable = false;
-        Class[] interfaces = cls.getInterfaces();
+        Class<?>[] interfaces = cls.getInterfaces();
         for (int i = 0; i < interfaces.length; i++) {
             if (interfaces[i].getName().equals(PersistenceCapable.class.getName())) {
                 isPersistenceCapable = true;

openjpa-tools/openjpa-maven-plugin/src/main/java/org/apache/openjpa/tools/maven/AbstractOpenJpaEnhancerMojo.java
@@ -26,6 +26,7 @@ import org.codehaus.plexus.util.FileUtils;
 import org.apache.openjpa.enhance.PCEnhancer;
 import org.apache.openjpa.lib.util.Options;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -89,7 +90,7 @@ public abstract class AbstractOpenJpaEnhancerMojo extends AbstractOpenJpaMojo {
             FileUtils.mkdir(getEntityClasses().getAbsolutePath());
         }
 
-        List entities = findEntityClassFiles();
+        List<File> entities = findEntityClassFiles();
 
         enhance(entities);
     }
@@ -117,7 +118,7 @@ public abstract class AbstractOpenJpaEnhancerMojo extends AbstractOpenJpaMojo {
      * @param files class file resources to enhance.
      * @throws MojoExecutionException if the enhancer encountered a failure
      */
-    private void enhance(List files) throws MojoExecutionException {
+    private void enhance(List<File> files) throws MojoExecutionException {
         Options opts = getOptions();
 
         // list of input files

openjpa-tools/openjpa-maven-plugin/src/main/java/org/apache/openjpa/tools/maven/AbstractOpenJpaMojo.java
@@ -25,16 +25,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.FileUtils;
-
 import org.apache.openjpa.lib.util.Options;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * Base class for  OpenJPA maven tasks.
@@ -141,7 +139,7 @@ public abstract class AbstractOpenJpaMojo extends AbstractMojo
      * @required
      * @readonly
      */
-    protected List compileClasspathElements;
+    protected List<String> compileClasspathElements;
     
     /**
      * Setting this parameter to <code>true</code> will force
@@ -199,7 +197,7 @@ public abstract class AbstractOpenJpaMojo extends AbstractMojo
      * This function retrieves the injected classpath elements for the current mojo.
      * @return List of classpath elements for the compile phase
      */
-    protected List getClasspathElements() 
+    protected List<String> getClasspathElements() 
     {
         return compileClasspathElements;
     }
@@ -278,11 +276,10 @@ public abstract class AbstractOpenJpaMojo extends AbstractMojo
     protected void extendRealmClasspath() 
         throws MojoExecutionException 
     { 
-        List urls = new ArrayList();
+        List<URL> urls = new ArrayList<URL>();
 
-        for ( Iterator itor = getClasspathElements().iterator(); itor.hasNext(); )
-        {
-            File pathElem = new File( (String) itor.next() );
+        for(String fileName: getClasspathElements()) { 
+            File pathElem = new File(fileName);
             try
             {
                 URL url = pathElem.toURI().toURL();
@@ -310,13 +307,13 @@ public abstract class AbstractOpenJpaMojo extends AbstractMojo
      * @throws MojoExecutionException if there was an error scanning class file
      *             resources.
      */
-    protected List findEntityClassFiles() throws MojoExecutionException 
+    protected List<File> findEntityClassFiles() throws MojoExecutionException 
     {
-        List files = new ArrayList();
+        List<File> files = new ArrayList<File>();
     
         try
         {
-            files = FileUtils.getFiles( getEntityClasses(), includes, excludes );
+            files = (List<File>) FileUtils.getFiles( getEntityClasses(), includes, excludes );
         }
         catch ( IOException e )
         {
@@ -331,12 +328,12 @@ public abstract class AbstractOpenJpaMojo extends AbstractMojo
      * @param files List of files
      * @return the paths of the given files as String[]
      */
-    protected String[] getFilePaths( List files ) 
+    protected String[] getFilePaths( List<File> files ) 
     {
         String[] args = new String[ files.size() ];
         for ( int i = 0; i < files.size(); i++ )
         {
-            File file = (File) files.get( i );
+            File file = files.get( i );
     
             args[ i ] = file.getAbsolutePath();
         }

openjpa-tools/openjpa-maven-plugin/src/main/java/org/apache/openjpa/tools/maven/OpenJpaTestEnhancerMojo.java
@@ -43,7 +43,7 @@ public class OpenJpaTestEnhancerMojo extends AbstractOpenJpaEnhancerMojo {
      * @required
      * @readonly
      */
-    protected List testClasspathElements;
+    protected List<String> testClasspathElements;
 
     /**
      * This is where compiled test classes go.
@@ -70,7 +70,7 @@ public class OpenJpaTestEnhancerMojo extends AbstractOpenJpaEnhancerMojo {
      *
      * @return List of classpath elements for the test phase
      */
-    protected List getClasspathElements() {
+    protected List<String> getClasspathElements() {
         return testClasspathElements;
     }
 

openjpa-tools/openjpa-maven-plugin/src/test/java/org/apache/openjpa/tools/maven/OpenJpaEnhancerMojoTest.java
@@ -42,7 +42,7 @@ public class OpenJpaEnhancerMojoTest extends AbstractMojoTestCase {
       assertNotNull( mojo );
       
       mojo.classes = new File( getBasedir(), "target/test-classes/" );
-      mojo.compileClasspathElements = new ArrayList();
+      mojo.compileClasspathElements = new ArrayList<String>();
       mojo.compileClasspathElements.add( mojo.classes.getAbsolutePath() );
       
       mojo.execute();

commit 9f4fba24dab254fb43d00ac65a3b2896d3ed9e61

OPENJPA-2100 fix filterPersistenceCapable
Fixing a bug which got introduced in r1091279 and caused entities
which contained enums to be broken.


git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1224892 13f79535-47bb-0310-9956-ffa450edef68

openjpa-tools/openjpa-maven-plugin/pom.xml
@@ -116,6 +116,10 @@
             <artifactId>geronimo-validation_1.0_spec</artifactId>
         </dependency>
         <dependency>
+            <groupId>org.apache.bval</groupId>
+            <artifactId>org.apache.bval.bundle</artifactId>
+        </dependency>
+        <dependency>
             <groupId>log4j</groupId>
             <artifactId>log4j</artifactId>
             <version>1.2.14</version>

openjpa-tools/openjpa-maven-plugin/src/it/default_settings/src/main/java/org/apache/openjpa/tools/maven/testentity/TestEntity.java
@@ -20,42 +20,60 @@ package org.apache.openjpa.tools.maven.testentity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
 
 
 @Entity
 public class TestEntity {
 
-  @Id
-  private int xint1;
+    @Id
+    private int xint1;
 
-  private String string1;
+    private String string1;
 
-  protected TestEntity() {
-  }
+    public enum SampleEnum {
+        Option1, Option2, Option3
+    }
 
-  public TestEntity(int int1, String string1) {
-    this.xint1 = int1;
-    this.string1 = string1;
-  }
+    @Enumerated(EnumType.STRING)
+    private SampleEnum myEnum;
 
-  public int getInt1() {
-    return xint1;
-  }
 
-  public void setInt1(int int1) {
-    this.xint1 = int1;
-  }
+    protected TestEntity() {
+    }
 
-  public String getString1() {
-    return string1;
-  }
+    public TestEntity(int int1, String string1) {
+        this.xint1 = int1;
+        this.string1 = string1;
+    }
 
-  public void setString1(String string1) {
-    this.string1 = string1;
-  }
+    public int getInt1() {
+        return xint1;
+    }
 
-  public String toString()  {
-    return xint1 + ":" + string1;
-  }
+    public void setInt1(int int1) {
+        this.xint1 = int1;
+    }
+
+    public String getString1() {
+        return string1;
+    }
+
+    public void setString1(String string1) {
+        this.string1 = string1;
+    }
+
+    public String toString()  {
+        return xint1 + ":" + string1;
+    }
+
+    public SampleEnum getMyEnum() {
+        return myEnum;
+    }
+
+    public void setMyEnum(SampleEnum myEnum) {
+        this.myEnum = myEnum;
+    }
 
 }

openjpa-tools/openjpa-maven-plugin/src/main/java/org/apache/openjpa/tools/maven/AbstractOpenJpaMappingToolMojo.java
@@ -22,6 +22,7 @@ package org.apache.openjpa.tools.maven;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -130,8 +131,11 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
         MetaDataRepository repo = conf.newMetaDataRepositoryInstance();
         ClassArgParser cap = repo.getMetaDataFactory().newClassArgParser();
 
-        for(File classPath : files) { 
-            Class<?>[] classes = cap.parseTypes(classPath.getAbsolutePath());
+        Iterator<File> fileIt = files.iterator();
+        while (fileIt.hasNext()) {
+            File classPath = fileIt.next();
+
+            Class[] classes = cap.parseTypes(classPath.getAbsolutePath());
 
             if (classes == null) {
                 getLog().info("Found no classes for " + classPath.getAbsolutePath());
@@ -146,6 +150,7 @@ public abstract class AbstractOpenJpaMappingToolMojo extends AbstractOpenJpaMojo
                                 + PersistenceCapable.class.getName());
                     } else {
                         getLog().debug("Removing non-entity class " + classPath);
+                        fileIt.remove();
                     }
                 }
             }

openjpa-tools/openjpa-maven-plugin/src/it/default_settings/src/test/java/org/apache/openjpa/tools/maven/test/ItDefaultSettingsTest.java
@@ -43,7 +43,8 @@ public class ItDefaultSettingsTest extends TestCase {
     private final static String SQL_FILE = "database.sql";
     
     /** if the SQL generation has been successful, the following result should be in the SQL file */
-    private final static String VALID_SQL = "CREATE TABLE TestEntity (xint1 INTEGER NOT NULL, string1 VARCHAR(255), PRIMARY KEY (xint1));";
+    private final static String VALID_SQL = "CREATE TABLE TestEntity (xint1 INTEGER NOT NULL, myEnum VARCHAR(20), "
+                                            + "string1 VARCHAR(255), PRIMARY KEY (xint1));";
     
     private final static String TEST_ENTITY_CLASS = "org.apache.openjpa.tools.maven.testentity.TestEntity";
 

openjpa-tools/openjpa-maven-plugin/src/it/default_settings/src/test/resources/reference_schema.orig_xml
@@ -4,6 +4,7 @@
         <table name="TestEntity">
             <pk column="xint1"/>
             <column name="xint1" type="integer" not-null="true"/>
+            <column name="myEnum" type="varchar" size="20"/>
             <column name="string1" type="varchar" size="255"/>
         </table>
     </schema>