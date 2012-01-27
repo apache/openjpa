11dc47c80ab3883b2da6eabd38d3af6130239fc7
Albert Lee <allee8285@apache.org>
2012-01-27 16:20:05 +0000

    OPENJPA-2117 - change setColumn gated on isSerialized not isElementCollection
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1236731 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/main/java/org/apache/openjpa/persistence/jdbc/XMLPersistenceMappingParser.java
+++ openjpa-persistence-jdbc/src/main/java/org/apache/openjpa/persistence/jdbc/XMLPersistenceMappingParser.java
@@ -729,7 +729,7 @@ public class XMLPersistenceMappingParser
                     }
                     // else no break
                 case JavaTypes.COLLECTION:
-                    if(fm.isElementCollection()) { 
+                if (!fm.getValue().isSerialized()) {
                         fm.getElementMapping().getValueInfo().setColumns(_cols);
                     } else  {
                         fm.getValueInfo().setColumns(_cols);
