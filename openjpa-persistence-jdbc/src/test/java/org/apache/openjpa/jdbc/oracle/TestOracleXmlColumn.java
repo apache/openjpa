a2c820b66ee9bfa380a2fbe73a032fc040b38a0d
Mark Struberg <struberg@apache.org>
2012-04-27 22:09:12 +0000

    replace tabs with spaces only
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1331613 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/jdbc/oracle/TestOracleXmlColumn.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/jdbc/oracle/TestOracleXmlColumn.java
@@ -37,11 +37,11 @@ public class TestOracleXmlColumn extends AbstractPersistenceTestCase {
     private static String projectStr = "project";
     private static String xmlData =
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-            + "<" + projectStr + " xmlns=\"http://maven.apache.org/POM/4.0.0\" " +
-            		"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
-            		"xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 " +
-            		"http://maven.apache.org/maven-v4_0_0.xsd\">"
-            + "</" + projectStr + ">";
+        + "<" + projectStr + " xmlns=\"http://maven.apache.org/POM/4.0.0\" "
+        + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
+        + "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 "
+        + "http://maven.apache.org/maven-v4_0_0.xsd\">"
+        + "</" + projectStr + ">";
 
     private boolean skipTest(DBDictionary dict) {
         return !(dict instanceof OracleDictionary);
