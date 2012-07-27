423d1d18e4066f4be13dc8d60bcf41759b5cfac8
Albert Lee <allee8285@apache.org>
2012-07-27 20:39:16 +0000

    OPENJPA-2231 - add proper imports.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1366526 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-jest/src/main/java/org/apache/openjpa/persistence/jest/TokenReplacedStream.java
+++ openjpa-jest/src/main/java/org/apache/openjpa/persistence/jest/TokenReplacedStream.java
@@ -19,8 +19,11 @@
 
 package org.apache.openjpa.persistence.jest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.Writer;
 import java.util.Arrays;
 
