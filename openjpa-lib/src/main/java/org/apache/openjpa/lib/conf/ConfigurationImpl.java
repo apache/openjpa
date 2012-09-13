7eb70bf98c0af96f2713a2599d825a4d8bf189f5
Jody Grassel <jgrassel@apache.org>
2012-09-13 22:28:00 +0000

    OPENJPA-428: Restore: Bad error message regarding openjpa.Id
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/2.1.x@1384572 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-lib/src/main/java/org/apache/openjpa/lib/conf/ConfigurationImpl.java
+++ openjpa-lib/src/main/java/org/apache/openjpa/lib/conf/ConfigurationImpl.java
@@ -665,6 +665,7 @@ public class ConfigurationImpl
         // <prefix>.properties System property; remove that property so we
         // we don't warn about it
         Configurations.removeProperty("properties", remaining);
+        Configurations.removeProperty("Id", remaining, map);
 
         // now warn if there are any remaining properties that there
         // is an unhandled prop, and remove the unknown properties
