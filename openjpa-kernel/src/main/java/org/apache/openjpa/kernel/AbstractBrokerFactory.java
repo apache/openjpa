4487017fbf57fb7aed4024e0991850bb89fc1c43
Jeremy Bauer <jrbauer@apache.org>
2013-03-28 21:43:39 +0000

    Disable logging during brokerfactory de-serialization.  Added type checking of plugin values.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/2.0.x@1462318 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-kernel/src/main/java/org/apache/openjpa/kernel/AbstractBrokerFactory.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/kernel/AbstractBrokerFactory.java
@@ -475,8 +475,14 @@ public abstract class AbstractBrokerFactory
         // reset these transient fields to empty values
         _transactional = new ConcurrentHashMap<Object,Collection<Broker>>();
         _brokers = newBrokerSet();
-
+        
+        // turn off logging while de-serializing BrokerFactory
+        String saveLogConfig = _conf.getLog();
+        _conf.setLog("none");
         makeReadOnly();
+        // re-enable any logging which was in effect
+        _conf.setLog(saveLogConfig);  
+        
         return this;
     }
 
