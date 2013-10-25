d081e053eb6a26f1320c25288c53138a273a3994
Mark Struberg <struberg@apache.org>
2013-10-25 19:45:08 +0000

    OPENJPA-2018 improve unit test setup
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/2.3.x@1535839 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/openjpa2018/OpenJPA2018Test.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/openjpa2018/OpenJPA2018Test.java
@@ -18,12 +18,8 @@
  */
 package org.apache.openjpa.openjpa2018;
 
-import junit.framework.TestCase;
-import org.apache.openjpa.persistence.OpenJPAPersistence;
+import org.apache.openjpa.persistence.test.SingleEMTestCase;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -33,87 +29,87 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class OpenJPA2018Test extends TestCase {
-    private EntityManager em;
-    private EntityTransaction transaction;
-    private EntityManagerFactory factory;
+public class OpenJPA2018Test extends SingleEMTestCase
+{
 
     @Override
     public void setUp() {
-        factory = OpenJPAPersistence.createEntityManagerFactory("openjpa2018", "META-INF/openjpa2018.xml");
-        em = factory.createEntityManager();
-        transaction = em.getTransaction();
-        transaction.begin();
-    }
-
-    @Override
-    public void tearDown() {
-        transaction.rollback();
-        em.close();
-        factory.close();
+        super.setUp(User2018.class, CLEAR_TABLES);
     }
 
     public void testInCriteriaWithArray() {
+        em.getTransaction().begin();
+        try {
+            User2018 user = new User2018();
+            em.persist(user);
+            em.flush();
 
-        User2018 user = new User2018();
-        em.persist(user);
-        em.flush();
+            CriteriaBuilder builder = em.getCriteriaBuilder();
 
-        CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
+            Root<User2018> root = criteria.from(User2018.class);
+            criteria.where(root.get("id").in(builder.parameter(Long[].class)));
 
-        CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
-        Root<User2018> root = criteria.from(User2018.class);
-        criteria.where(root.get("id").in(builder.parameter(Long[].class)));
+            TypedQuery<User2018> query = em.createQuery(criteria);
+            for (ParameterExpression parameter : criteria.getParameters()) {
+                query.setParameter(parameter, new Long[] { user.id });
+            }
 
-        TypedQuery<User2018> query = em.createQuery(criteria);
-        for (ParameterExpression parameter : criteria.getParameters()) {
-            query.setParameter(parameter, new Long[] { user.id });
+            List<User2018> result = query.getResultList();
+            assertTrue(!result.isEmpty());
+        } finally {
+            em.getTransaction().commit();
         }
-
-        List<User2018> result = query.getResultList();
-        assertTrue(!result.isEmpty());
     }
 
     public void testInCriteriaWithCollection() {
+        em.getTransaction().begin();
+        try {
+            User2018 user = new User2018();
+            em.persist(user);
+            em.flush();
 
-        User2018 user = new User2018();
-        em.persist(user);
-        em.flush();
+            CriteriaBuilder builder = em.getCriteriaBuilder();
 
-        CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
+            Root<User2018> root = criteria.from(User2018.class);
+            criteria.where(root.get("id").in(builder.parameter(Collection.class)));
 
-        CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
-        Root<User2018> root = criteria.from(User2018.class);
-        criteria.where(root.get("id").in(builder.parameter(Collection.class)));
+            TypedQuery<User2018> query = em.createQuery(criteria);
+            for (ParameterExpression parameter : criteria.getParameters()) {
+                query.setParameter(parameter, Arrays.asList(user.id));
+            }
 
-        TypedQuery<User2018> query = em.createQuery(criteria);
-        for (ParameterExpression parameter : criteria.getParameters()) {
-            query.setParameter(parameter, Arrays.asList(user.id));
+            List<User2018> result = query.getResultList();
+            assertTrue(!result.isEmpty());
+        } finally {
+            em.getTransaction().commit();
         }
-
-        List<User2018> result = query.getResultList();
-        assertTrue(!result.isEmpty());
     }
 
     public void testId() {
+        em.getTransaction().begin();
+        try {
+            User2018 user = new User2018();
+            em.persist(user);
+            em.flush();
 
-        User2018 user = new User2018();
-        em.persist(user);
-        em.flush();
+            CriteriaBuilder builder = em.getCriteriaBuilder();
 
-        CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
+            Root<User2018> root = criteria.from(User2018.class);
+            criteria.where(builder.equal(root.get("id"), user.id));
 
-        CriteriaQuery<User2018> criteria = builder.createQuery(User2018.class);
-        Root<User2018> root = criteria.from(User2018.class);
-        criteria.where(builder.equal(root.get("id"), user.id));
+            TypedQuery<User2018> query = em.createQuery(criteria);
+            for (ParameterExpression parameter : criteria.getParameters()) {
+                query.setParameter(parameter, user.id);
+            }
 
-        TypedQuery<User2018> query = em.createQuery(criteria);
-        for (ParameterExpression parameter : criteria.getParameters()) {
-            query.setParameter(parameter, user.id);
+            List<User2018> result = query.getResultList();
+            assertTrue(!result.isEmpty());
+        } finally {
+            em.getTransaction().commit();
         }
-
-        List<User2018> result = query.getResultList();
-        assertTrue(!result.isEmpty());
     }
 
 }
