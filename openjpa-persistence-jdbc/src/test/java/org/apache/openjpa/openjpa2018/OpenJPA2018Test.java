092e70e0ac381227bcf55974000a48203cecd980
Mark Struberg <struberg@apache.org>
2013-10-28 17:43:21 +0000

    OPENJPA-2018 fix handling of Arrays in Select IN statements
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1536439 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/openjpa2018/OpenJPA2018Test.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/openjpa2018/OpenJPA2018Test.java
@@ -29,6 +29,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * This test verifies that select IN statements with
+ * arrays and Collections are fine.
+ */
 public class OpenJPA2018Test extends SingleEMTestCase
 {
 
@@ -52,8 +56,26 @@ public class OpenJPA2018Test extends SingleEMTestCase
 
             TypedQuery<User2018> query = em.createQuery(criteria);
             for (ParameterExpression parameter : criteria.getParameters()) {
-                query.setParameter(parameter, new Long[] { user.id });
+                query.setParameter(parameter, new Long[] { user.id, 123456789L });
             }
+
+            List<User2018> result = query.getResultList();
+            assertTrue(!result.isEmpty());
+        } finally {
+            em.getTransaction().commit();
+        }
+    }
+
+    public void testInJpqlWithArray() {
+        em.getTransaction().begin();
+        try {
+            User2018 user = new User2018();
+            em.persist(user);
+            em.flush();
+
+            TypedQuery<User2018> query = em.createQuery("select u from User2018 as u where u.id in (:userIds)",
+                                                        User2018.class);
+            query.setParameter("userIds", new Long[] { user.id, 123456789L });
 
             List<User2018> result = query.getResultList();
             assertTrue(!result.isEmpty());
@@ -77,7 +99,7 @@ public class OpenJPA2018Test extends SingleEMTestCase
 
             TypedQuery<User2018> query = em.createQuery(criteria);
             for (ParameterExpression parameter : criteria.getParameters()) {
-                query.setParameter(parameter, Arrays.asList(user.id));
+                query.setParameter(parameter, Arrays.asList(user.id, 123456789L));
             }
 
             List<User2018> result = query.getResultList();
@@ -86,6 +108,25 @@ public class OpenJPA2018Test extends SingleEMTestCase
             em.getTransaction().commit();
         }
     }
+
+    public void testInJpqlWithCollection() {
+        em.getTransaction().begin();
+        try {
+            User2018 user = new User2018();
+            em.persist(user);
+            em.flush();
+
+            TypedQuery<User2018> query = em.createQuery("select u from User2018 as u where u.id in (:userIds)",
+                    User2018.class);
+            query.setParameter("userIds", Arrays.asList(user.id, 123456789L));
+
+            List<User2018> result = query.getResultList();
+            assertTrue(!result.isEmpty());
+        } finally {
+            em.getTransaction().commit();
+        }
+    }
+
 
     public void testId() {
         em.getTransaction().begin();
