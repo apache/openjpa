bb81584a439177f423522997aa6ac19fe7220604
Heath Thomann <hthomann@apache.org>
2014-06-12 03:55:29 +0000

    OPENJPA-2508: Account for JOIN FETCH statements when loading from the Query Cache.  Back ported to 2.3.x Rick's trunk commit.
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/branches/2.3.x@1602060 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/jpql/joins/TestJoinFetchWithQueryDataCache.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/jpql/joins/TestJoinFetchWithQueryDataCache.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.JoinType;
@@ -29,196 +30,217 @@ import javax.persistence.criteria.Root;
 
 import junit.framework.Assert;
 
-import org.apache.openjpa.persistence.test.SingleEMFTestCase;
+import org.apache.openjpa.persistence.OpenJPAEntityManager;
+import org.apache.openjpa.persistence.OpenJPAPersistence;
+import org.apache.openjpa.persistence.test.SQLListenerTestCase;
 
 /**
  * Tests JQPL and Criteria API equivalent using left join fetch with QueryCache and DataCache enabled.
  */
-public class TestJoinFetchWithQueryDataCache extends SingleEMFTestCase {
+public class TestJoinFetchWithQueryDataCache extends SQLListenerTestCase {
     EntityManager em;
-        public void setUp() {
-        super.setUp(DROP_TABLES, Employee.class, Department.class
-                , "openjpa.QueryCompilationCache", "all"
-                , "openjpa.DataCache", "true(CacheSize=2000, SoftReferenceSize=0, EvictionSchedule='+60' )"
-                , "openjpa.RemoteCommitProvider", "sjvm"
-                , "openjpa.QueryCache", "true"
-                        );
 
-                em = emf.createEntityManager();
-                em.getTransaction().begin();
+    public void setUp() {
+        super.setUp(DROP_TABLES, Employee.class, Department.class, "openjpa.QueryCompilationCache", "all",
+            "openjpa.DataCache", "true", "openjpa.RemoteCommitProvider", "sjvm", "openjpa.QueryCache", "true"
+            // This is a hack to work around using em.detach(...) w/ a 1.0 p.xml
+            , "openjpa.Compatibility", "CopyOnDetach=false" 
+            );
 
-                Department dept;
-                dept = new Department(10, "department 10");
-                dept.setEmployees(new ArrayList<Employee>());
-                dept.getEmployees().add(new Employee(11, "Emp11"));
-                dept.getEmployees().add(new Employee(12, "Emp12"));
-                dept.setEmployee2s(new ArrayList<Employee>());
-                dept.getEmployee2s().add(new Employee(211, "Emp211"));
-                dept.getEmployee2s().add(new Employee(212, "Emp212"));
-                em.persist(dept);
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
 
-                dept = new Department(20, "department 20");
-                dept.setEmployees(new ArrayList<Employee>());
-                dept.getEmployees().add(new Employee(21, "Emp21"));
-                dept.getEmployees().add(new Employee(22, "Emp22"));
-                dept.setEmployee2s(new ArrayList<Employee>());
-                dept.getEmployee2s().add(new Employee(221, "Emp221"));
-                dept.getEmployee2s().add(new Employee(222, "Emp222"));
-                em.persist(dept);
+        Department dept;
+        dept = new Department(10, "department 10");
+        dept.setEmployees(new ArrayList<Employee>());
+        dept.getEmployees().add(new Employee(11, "Emp11", dept));
+        dept.getEmployees().add(new Employee(12, "Emp12", dept));
+        dept.setEmployee2s(new ArrayList<Employee>());
+        dept.getEmployee2s().add(new Employee(211, "Emp211", dept));
+        dept.getEmployee2s().add(new Employee(212, "Emp212", dept));
+        em.persist(dept);
 
-                em.getTransaction().commit();
+        dept = new Department(20, "department 20");
+        dept.setEmployees(new ArrayList<Employee>());
+        dept.getEmployees().add(new Employee(21, "Emp21", dept));
+        dept.getEmployees().add(new Employee(22, "Emp22", dept));
+        dept.setEmployee2s(new ArrayList<Employee>());
+        dept.getEmployee2s().add(new Employee(221, "Emp221", dept));
+        dept.getEmployee2s().add(new Employee(222, "Emp222", dept));
+        em.persist(dept);
 
-                em.close();
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void testJPQLNoFetch() {
+        EntityManager em = emf.createEntityManager();
+        List<Department> ds = em.createQuery("SELECT DISTINCT d FROM Department d WHERE d.deptno = 10").getResultList();
+        System.out.println("-- testJPQLNoFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNull(x.getEmployees());
+            Assert.assertNull(x.getEmployee2s());
+            System.out.println(x);
         }
 
-        public void testJPQLNoFetch() {
-                EntityManager em = emf.createEntityManager();
-                List<Department> ds = em.createQuery(
-                                "SELECT DISTINCT d FROM Department d WHERE d.deptno = 10")
-                                .getResultList();
-                System.out.println("-- testJPQLNoFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNull(x.getEmployees());
-                        Assert.assertNull(x.getEmployee2s());
-                        System.out.println(x);
-                }
+        em.close();
+    }
 
-                em.close();
+    public void testJPQLOneFetch() {
+        EntityManager em = emf.createEntityManager();
+        List<Department> ds =
+            em.createQuery("SELECT DISTINCT d FROM Department d LEFT JOIN FETCH d.employee2s " + "WHERE d.deptno = 10")
+                .getResultList();
+        System.out.println("-- testJPQLOneFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNull(x.getEmployees());
+            Assert.assertNotNull(x.getEmployee2s());
+            Assert.assertEquals(2, x.getEmployee2s().size());
+            System.out.println(x);
         }
 
-        public void testJPQLOneFetch() {
-                EntityManager em = emf.createEntityManager();
-                List<Department> ds = em.createQuery(
-                                "SELECT DISTINCT d FROM Department d LEFT JOIN FETCH d.employee2s " +
-                                        "WHERE d.deptno = 10")
-                                .getResultList();
-                System.out.println("-- testJPQLOneFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNull(x.getEmployees());
-                        Assert.assertNotNull(x.getEmployee2s());
-                        Assert.assertEquals(2, x.getEmployee2s().size());
-                        System.out.println(x);
-                }
+        em.close();
+    }
 
-                em.close();
+    public void testJPQLTwoFetch() {
+        EntityManager em = emf.createEntityManager();
+        List<Department> ds =
+            em.createQuery(
+                "SELECT DISTINCT d FROM Department d LEFT JOIN FETCH d.employees " + "LEFT JOIN FETCH d.employee2s "
+                    + "WHERE d.deptno = 10").getResultList();
+        System.out.println("-- testJPQLTwoFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNotNull(x.getEmployees());
+            Assert.assertEquals(2, x.getEmployees().size());
+            Assert.assertNotNull(x.getEmployee2s());
+            Assert.assertEquals(2, x.getEmployee2s().size());
+            System.out.println(x);
         }
 
-        public void testJPQLTwoFetch() {
-                EntityManager em = emf.createEntityManager();
-                List<Department> ds = em.createQuery(
-                                "SELECT DISTINCT d FROM Department d LEFT JOIN FETCH d.employees " +
-                            "LEFT JOIN FETCH d.employee2s " +
-                                        "WHERE d.deptno = 10")
-                                .getResultList();
-                System.out.println("-- testJPQLTwoFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNotNull(x.getEmployees());
-                        Assert.assertEquals(2, x.getEmployees().size());
-                        Assert.assertNotNull(x.getEmployee2s());
-                        Assert.assertEquals(2, x.getEmployee2s().size());
-                        System.out.println(x);
-                }
+        em.close();
+    }
 
-                em.close();
+    public void testCriteriaAPINoFetch() {
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        // This query is equivalent to the following Java Persistence query
+        // language query:
+        // SELECT d
+        // FROM Department d
+        // WHERE d.deptno = 1
+
+        CriteriaQuery<Department> q = cb.createQuery(Department.class);
+        Root<Department> d = q.from(Department.class);
+        q.where(cb.equal(d.get(Department_.deptno), 20)).select(d);
+
+        List<Department> ds = em.createQuery(q).getResultList();
+        System.out.println("-- testCriteriaAPINoFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNull(x.getEmployees());
+            Assert.assertNull(x.getEmployee2s());
+            System.out.println(x);
         }
 
-        public void testCriteriaAPINoFetch() {
-                EntityManager em = emf.createEntityManager();
-                CriteriaBuilder cb = em.getCriteriaBuilder();
+        em.close();
+    }
 
-                // This query is equivalent to the following Java Persistence query
-                // language query:
-                // SELECT d
-                // FROM Department d
-                // WHERE d.deptno = 1
+    public void testCriteriaAPIOneFetch() {
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
 
-                CriteriaQuery<Department> q = cb.createQuery(Department.class);
-                Root<Department> d = q.from(Department.class);
-                q.where(cb.equal(d.get(Department_.deptno), 20)).select(d);
+        // 6.5.4 Fetch Joins
+        // Example:
+        // CriteriaQuery<Department> q = cb.createQuery(Department.class);
+        // Root<Department> d = q.from(Department.class);
+        // d.fetch(Department_.employees, JoinType.LEFT);
+        // q.where(cb.equal(d.get(Department_.deptno), 1)).select(d);
+        //
+        // This query is equivalent to the following Java Persistence query
+        // language query:
+        // SELECT DISTINCT d
+        // FROM Department d LEFT JOIN FETCH d.employees
+        // WHERE d.deptno = 1
 
-                List<Department> ds = em.createQuery(q).getResultList();
-                System.out.println("-- testCriteriaAPINoFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNull(x.getEmployees());
-                        Assert.assertNull(x.getEmployee2s());
-                        System.out.println(x);
-                }
+        CriteriaQuery<Department> q = cb.createQuery(Department.class);
+        Root<Department> d = q.from(Department.class);
+        d.fetch(Department_.employees, JoinType.LEFT);
+        q.where(cb.equal(d.get(Department_.deptno), 20)).select(d).distinct(true);
 
-                em.close();
+        List<Department> ds = em.createQuery(q).getResultList();
+        System.out.println("-- testCriteriaAPIOneFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNotNull(x.getEmployees());
+            Assert.assertEquals(2, x.getEmployees().size());
+            Assert.assertNull(x.getEmployee2s());
+            System.out.println(x);
         }
 
-        public void testCriteriaAPIOneFetch() {
-                EntityManager em = emf.createEntityManager();
-                CriteriaBuilder cb = em.getCriteriaBuilder();
+        em.close();
+    }
 
-                // 6.5.4 Fetch Joins
-                // Example:
-                // CriteriaQuery<Department> q = cb.createQuery(Department.class);
-                // Root<Department> d = q.from(Department.class);
-                // d.fetch(Department_.employees, JoinType.LEFT);
-                // q.where(cb.equal(d.get(Department_.deptno), 1)).select(d);
-                //
-                // This query is equivalent to the following Java Persistence query
-                // language query:
-                // SELECT DISTINCT d
-                // FROM Department d LEFT JOIN FETCH d.employees
-                // WHERE d.deptno = 1
+    public void testCriteriaAPITwoFetch() {
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
 
-                CriteriaQuery<Department> q = cb.createQuery(Department.class);
-                Root<Department> d = q.from(Department.class);
-                d.fetch(Department_.employees, JoinType.LEFT);
-                q.where(cb.equal(d.get(Department_.deptno), 20)).select(d).distinct(true);
+        // This query is equivalent to the following Java Persistence query
+        // language query:
+        // SELECT DISTINCT d
+        // FROM Department d LEFT JOIN FETCH d.employees LEFT JOIN FETCH d.employee2s
+        // WHERE d.deptno = 1
+        CriteriaQuery<Department> q = cb.createQuery(Department.class);
+        Root<Department> d = q.from(Department.class);
+        d.fetch(Department_.employees, JoinType.LEFT);
+        d.fetch(Department_.employee2s, JoinType.LEFT);
+        q.where(cb.equal(d.get(Department_.deptno), 20)).select(d).distinct(true);
 
-                List<Department> ds = em.createQuery(q).getResultList();
-                System.out.println("-- testCriteriaAPIOneFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNotNull(x.getEmployees());
-                        Assert.assertEquals(2, x.getEmployees().size());
-                        Assert.assertNull(x.getEmployee2s());
-                        System.out.println(x);
-                }
-
-                em.close();
+        List<Department> ds = em.createQuery(q).getResultList();
+        System.out.println("-- testCriteriaAPITwoFetch -----");
+        em.clear();
+        Assert.assertEquals(1, ds.size());
+        for (Department x : ds) {
+            Assert.assertNotNull(x.getEmployees());
+            Assert.assertEquals(2, x.getEmployees().size());
+            Assert.assertNotNull(x.getEmployee2s());
+            Assert.assertEquals(2, x.getEmployee2s().size());
+            System.out.println(x);
         }
 
-        public void testCriteriaAPITwoFetch() {
-                EntityManager em = emf.createEntityManager();
-                CriteriaBuilder cb = em.getCriteriaBuilder();
+        em.close();
+    }
 
-                // This query is equivalent to the following Java Persistence query
-                // language query:
-                // SELECT DISTINCT d
-                // FROM Department d LEFT JOIN FETCH d.employees LEFT JOIN FETCH d.employee2s
-                // WHERE d.deptno = 1
-                CriteriaQuery<Department> q = cb.createQuery(Department.class);
-                Root<Department> d = q.from(Department.class);
-                d.fetch(Department_.employees, JoinType.LEFT);
-                d.fetch(Department_.employee2s, JoinType.LEFT);
-                q.where(cb.equal(d.get(Department_.deptno), 20)).select(d).distinct(true);
+    public void testConsecutiveJPQLJoinFetchCall() {
+        doQuery(emf, false);
+        doQuery(emf, true);
+    }
 
-                List<Department> ds = em.createQuery(q).getResultList();
-                System.out.println("-- testCriteriaAPITwoFetch -----");
-                em.clear();
-                Assert.assertEquals(1, ds.size());
-                for (Department x : ds) {
-                        Assert.assertNotNull(x.getEmployees());
-                        Assert.assertEquals(2, x.getEmployees().size());
-                        Assert.assertNotNull(x.getEmployee2s());
-                        Assert.assertEquals(2, x.getEmployee2s().size());
-                        System.out.println(x);
-                }
+    private void doQuery(EntityManagerFactory emf, boolean cached) {
+        String query = "select o from Employee o " + "left join fetch o.dept " + "where o.dept.deptno = 10";
+        EntityManager em = emf.createEntityManager();
 
-                em.close();
+        sql.clear();
+        List<Employee> emps = em.createQuery(query, Employee.class).getResultList();
+        Assert.assertEquals(4, emps.size());
+        for (Employee emp : emps) {
+            em.detach(emp);
+
+            Assert.assertNotNull(emp.getDept());
+            Assert.assertEquals(2, emp.getDept().getEmployees().size());
         }
-
+        em.close();
+        if (cached) {
+            assertTrue(sql.size() == 0);
+        }
+    }
 }
