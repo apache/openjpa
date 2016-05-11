bde39c4bc9f2303f4da6963601728454450be10f
Mark Struberg <struberg@apache.org>
2016-05-11 11:32:59 +0000

    OPENJPA-2641 upgrade build poms to most recent dependency versions
    git-svn-id: https://svn.apache.org/repos/asf/openjpa/trunk@1743338 13f79535-47bb-0310-9956-ffa450edef68

--- openjpa-tools/openjpa-maven-plugin/src/it/default_settings/src/test/java/org/apache/openjpa/tools/maven/test/ItDefaultSettingsTest.java
+++ openjpa-tools/openjpa-maven-plugin/src/it/default_settings/src/test/java/org/apache/openjpa/tools/maven/test/ItDefaultSettingsTest.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
