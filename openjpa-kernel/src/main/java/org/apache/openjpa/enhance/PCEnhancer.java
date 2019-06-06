711ad2145ac1efdf7ae0edcf2c86f7bc03313b56
Will Dazey <dazeydev.3@gmail.com>
2019-06-06 14:48:52 -0500

    Convert build from SVN to GIT (#49)
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- openjpa-kernel/src/main/java/org/apache/openjpa/enhance/PCEnhancer.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/enhance/PCEnhancer.java
@@ -62,7 +62,6 @@ import org.apache.openjpa.lib.util.Localizer;
 import org.apache.openjpa.lib.util.Options;
 import org.apache.openjpa.lib.util.Services;
 import org.apache.openjpa.lib.util.Localizer.Message;
-import org.apache.openjpa.lib.util.svn.SVNUtils;
 import org.apache.openjpa.meta.AccessCode;
 import org.apache.openjpa.meta.ClassMetaData;
 import org.apache.openjpa.meta.FieldMetaData;
@@ -181,8 +180,7 @@ public class PCEnhancer {
                     in.close();
                 }
             }
-            String prop = revisionProps.getProperty("openjpa.enhancer.revision");
-            rev = SVNUtils.svnInfoToInteger(prop);
+            rev = Integer.parseInt(revisionProps.getProperty("openjpa.enhancer.revision"));
         } catch (Exception e) {
         }
         if (rev > 0) {
