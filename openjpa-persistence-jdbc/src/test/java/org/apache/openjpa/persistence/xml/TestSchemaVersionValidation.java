47be924ae95d0c47eb7a68c1ae35e9eefff07630
Maxim Solodovnik <solomax666@gmail.com>
2021-12-09 23:56:33 +0700

    Tests are added

--- openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/xml/TestSchemaVersionValidation.java
+++ openjpa-persistence-jdbc/src/test/java/org/apache/openjpa/persistence/xml/TestSchemaVersionValidation.java
@@ -21,19 +21,16 @@ package org.apache.openjpa.persistence.xml;
 import org.apache.openjpa.persistence.OpenJPAEntityManager;
 import org.apache.openjpa.persistence.OpenJPAEntityManagerFactory;
 import org.apache.openjpa.persistence.OpenJPAPersistence;
+import org.apache.openjpa.util.GeneralException;
+import org.junit.Test;
 
-import junit.framework.TestCase;
-
-public class TestSchemaVersionValidation extends TestCase {
-
+public class TestSchemaVersionValidation {
     /**
      * Verify a pu can be created with a version 2.0 persistence.xml
      */
+    @Test
     public void test2_0PersistenceXml() {
-        OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
-            createEntityManagerFactory("XSDTest",
-                "org/apache/openjpa/persistence/xml/persistence-2_0.xml");
-        emf.close();
+        doCreateEmf("XSDTest", "persistence-2_0.xml", false);
     }
 
     /**
@@ -42,61 +39,68 @@ public class TestSchemaVersionValidation extends TestCase {
      * (the 2.0 spec made it a requirement for the persistence file
      * to contain at least one pu.)
      */
+    // JREs fail differently for this test. Detection and
+    // assertion of a JRE specific failure has shown to be error prone
+    // so only a generic exception is detected.
+    @Test(expected = Exception.class)
     public void testBad2_0PersistenceXml() {
-        try {
-            OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
-                createEntityManagerFactory(null,
-                "org/apache/openjpa/persistence/xml/persistence-2_0-no-pu.xml");
-            emf.close();
-            fail();
-        } catch (Exception e) {
-            // JREs fail differently for this test. Detection and
-            // assertion of a JRE specific failure has shown to be error prone
-            // so only a generic exception is detected.
-        }
+        doCreateEmf(null, "persistence-2_0-no-pu.xml", false);
     }
 
     /**
      * Verify a version 2.0 persistence.xml can reference and the provider
      * can parse a version 1.0 orm.xml
      */
+    @Test
     public void test2_0Persistence1_0OrmXml() {
-        OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
-            createEntityManagerFactory("XSDTest",
-            "org/apache/openjpa/persistence/xml/" +
-            "persistence-2_0-orm-1_0.xml");
-        OpenJPAEntityManager em = emf.createEntityManager();
-        em.close();
-        emf.close();
+        doCreateEmf("XSDTest", "persistence-2_0-orm-1_0.xml");
     }
 
     /**
      * Verify a version 2.0 persistence.xml can reference and the provider can
      * parse a version 2.0 orm.xml
      */
+    @Test
     public void test2_0Persistence2_0OrmXml() {
-        OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
-            createEntityManagerFactory("XSDTest",
-            "org/apache/openjpa/persistence/xml/" +
-            "persistence-2_0-orm-2_0.xml");
-        OpenJPAEntityManager em = emf.createEntityManager();
-        em.close();
-        emf.close();
+        doCreateEmf("XSDTest", "persistence-2_0-orm-2_0.xml");
     }
-
-
 
     /**
      * Verify a 1.0 persistence.xml can include a 2.0 orm.xml
      */
+    @Test
     public void test1_0Persistence2_0OrmXml() {
-        OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
-            createEntityManagerFactory("XSDTest",
-            "org/apache/openjpa/persistence/xml/" +
-            "persistence-2_0-orm-1_0.xml");
-        OpenJPAEntityManager em = emf.createEntityManager();
-        em.close();
-        emf.close();
+        doCreateEmf("XSDTest", "persistence-2_0-orm-1_0.xml");
     }
 
+    /**
+     * Verify a version 66.6 persistence.xml with bad URL cannot be loaded
+     */
+    @Test(expected = GeneralException.class)
+    public void testOffline66_6Persistence2_2OrmXml() {
+        doCreateEmf("XSDTest", "offline_persistence-66_6-orm-2_2.xml");
+    }
+
+    /**
+     * Verify a version 2.1 persistence.xml with bad URL can reference and the provider can
+     * parse a version 2.2 orm.xml with bad URL 
+     */
+    @Test
+    public void testOffline2_1Persistence2_2OrmXml() {
+        doCreateEmf("XSDTest", "offline_persistence-2_1-orm-2_2.xml");
+    }
+
+    private void doCreateEmf(String name, String xml) {
+        doCreateEmf(name, xml, true);
+    }
+
+    private void doCreateEmf(String name, String xml, boolean createEm) {
+        OpenJPAEntityManagerFactory emf = OpenJPAPersistence.
+                createEntityManagerFactory(name, "org/apache/openjpa/persistence/xml/" + xml);
+        if (createEm) {
+            OpenJPAEntityManager em = emf.createEntityManager();
+            em.close();
+        }
+        emf.close();
+    }
 }
