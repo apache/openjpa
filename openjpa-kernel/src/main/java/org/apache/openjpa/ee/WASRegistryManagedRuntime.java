f19ba018e23ddbf8cdc1eef6d505340b705b257b
Mark Struberg <struberg@apache.org>
2023-05-15 09:58:34 +0200

    OPENJPA-2910 get rid of reflection
    UOWAction stuff is now also freely available, so no
    need for dirty hacks anymore.

--- openjpa-kernel/src/main/java/org/apache/openjpa/ee/WASRegistryManagedRuntime.java
+++ openjpa-kernel/src/main/java/org/apache/openjpa/ee/WASRegistryManagedRuntime.java
@@ -19,33 +19,21 @@
 package org.apache.openjpa.ee;
 
 import com.ibm.wsspi.uow.UOWAction;
-
-import java.lang.reflect.Method;
+import com.ibm.wsspi.uow.UOWManager;
+import com.ibm.wsspi.uow.UOWManagerFactory;
 
 /**
  * WASRegistryManagedRuntime provides WebSphere specific extensions to
- * {@link RegistryManagedRuntime}. Currently these extensions consist of using
- * the WebSphere UOWManager interface to submit non transactional work.
+ * {@link RegistryManagedRuntime}. Currently, these extensions consist of using
+ * the WebSphere UOWManager interface to submit non-transactional work.
  */
 public class WASRegistryManagedRuntime extends RegistryManagedRuntime {
 
     // value taken from com.ibm.websphere.uow.UOWSynchronizationRegistry
     private static final int WEBSPHERE_UOW_TYPE_LOCAL_TRANSACTION = 0;
 
-    private final Method getUOWManager;
-    private final Method runUnderUOW;
 
     public WASRegistryManagedRuntime() {
-        try {
-            Class classUOWManagerFactory = Class.forName("com.ibm.wsspi.uow.UOWManagerFactory");
-            getUOWManager = classUOWManagerFactory.getMethod("getUOWManager");
-
-            Class classUOWManager = Class.forName("com.ibm.wsspi.uow.UOWManager");
-            runUnderUOW = classUOWManager.getMethod("runUnderUOW", new Class[]{int.class, boolean.class, UOWAction.class});
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Problem while creating WASManagedRuntime", e);
-        }
     }
 
     /**
@@ -55,13 +43,10 @@ public class WASRegistryManagedRuntime extends RegistryManagedRuntime {
      * </P>
      */
     @Override
-    public void doNonTransactionalWork(Runnable runnable)
-            throws RuntimeException, UnsupportedOperationException {
+    public void doNonTransactionalWork(Runnable runnable) throws RuntimeException {
         try {
-            Object uowManager = getUOWManager.invoke(null);
-
-            runUnderUOW.invoke(uowManager, WEBSPHERE_UOW_TYPE_LOCAL_TRANSACTION, false, new DelegatingUOWAction(runnable));
-
+            UOWManager uowManager = UOWManagerFactory.getUOWManager();
+            uowManager.runUnderUOW(WEBSPHERE_UOW_TYPE_LOCAL_TRANSACTION, false, new DelegatingUOWAction(runnable));
         }
         catch(Exception e ) {
             RuntimeException re = new RuntimeException(e.getMessage(), e);
