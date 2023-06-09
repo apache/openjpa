2d9fe35b2134d8429ee08f8b4e5c9802d85447f0
Simone Staccone <simone.staccone@virgilio.it>
2023-06-09 02:09:37 +0200

    First dummy unit test with jacoco working

---
+++ openjpa-persistence/src/test/java/org/apache/openjpa/persistence/EntityManagerFactoryImplTest.java
@@ -0,0 +1,37 @@
+package org.apache.openjpa.persistence;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class EntityManagerFactoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBrokerFactory() {
+//        EntityManagerFactoryImpl entityManagerFactory = new EntityManagerFactoryImpl();
+//        BrokerFactory brokenFactory = entityManagerFactory.getBrokerFactory();
+//        brokenFactory.close();;
+//        Assertions.assertNotNull(brokenFactory);
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void setBrokerFactory() {
+    }
+
+    @Test
+    void createEntityManager() {
+    }
+
+}
