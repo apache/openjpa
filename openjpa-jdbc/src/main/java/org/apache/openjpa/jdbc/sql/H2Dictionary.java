f70cac36f83cdd7ac65cdbb85724d024c360579a
Maxim Solodovnik <solomax@apache.org>
2026-06-10 13:27:52 +0700

    H2-2 tests are fixed; code clean-up

--- openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/H2Dictionary.java
+++ openjpa-jdbc/src/main/java/org/apache/openjpa/jdbc/sql/H2Dictionary.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.apache.openjpa.jdbc.identifier.DBIdentifier;
+import org.apache.openjpa.jdbc.identifier.DBIdentifier.DBIdentifierType;
 import org.apache.openjpa.jdbc.kernel.exps.FilterValue;
 import org.apache.openjpa.jdbc.schema.Column;
 import org.apache.openjpa.jdbc.schema.PrimaryKey;
@@ -153,7 +154,7 @@ public class H2Dictionary extends DBDictionary {
             "WITH",
             "YEAR",
             "_ROWID_");
-    
+
     private final static List<String> V2_META_TABLES_NAMES = Arrays
     		.asList("INFORMATION_SCHEMA.INDEXES", "INFORMATION_SCHEMA.INDEX_COLUMNS", "INFORMATION_SCHEMA.INFORMATION_SCHEMA_CATALOG_NAME",
     				"INFORMATION_SCHEMA.ROLES", "INFORMATION_SCHEMA.SESSIONS", "INFORMATION_SCHEMA.SESSION_STATE",
@@ -266,6 +267,8 @@ public class H2Dictionary extends DBDictionary {
             reservedWordSet.addAll(V2_KEYWORDS);
             invalidColumnWordSet.clear();
             invalidColumnWordSet.addAll(V2_KEYWORDS);
+            getDefaultIdentifierRule().setReservedWords(reservedWordSet);
+            getIdentifierRules().get(DBIdentifierType.COLUMN.name()).setReservedWords(invalidColumnWordSet);
         }
     }
 
@@ -487,7 +490,7 @@ public class H2Dictionary extends DBDictionary {
         }
         return super.isFatalException(subtype, ex);
     }
-    
+
     @Override
     public String[] getDeleteTableContentsSQL(Table[] tables, Connection conn) {
     	if (versionLaterThan(1)) {
@@ -499,5 +502,5 @@ public class H2Dictionary extends DBDictionary {
     		return super.getDeleteTableContentsSQL(tables, conn);
     	}
     }
-    
+
 }
